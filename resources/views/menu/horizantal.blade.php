3f69798061c1dfa57055f854c5c30cf8bf0c2d39
quang <quangphuong9685@gmail.com>
2019-05-06 22:19:37 +0700

    phan quyen menu

--- resources/views/menu/horizantal.blade.php
+++ resources/views/menu/horizantal.blade.php
@@ -8,25 +8,27 @@
                     <span>Quản Lý Người Dùng</span>
                 </a>
             </li>
-            <li class="nav-item with-sub" {{ Request::is('category*')||Request::is('services*') ? 'active' : '' }}>
-                <a class="nav-link" href="#"><i class="fas fa-newspaper"></i><span>Quản lý dịch vụ</span></a>
-                <div class="sub-item">
-                    <ul>
-                        <li>
-                            <a href="{!! route('category.index') !!}">Danh mục dịch vụ</a>
-                        </li>
-                        <li>
-                            <a href="{!! route('services.index') !!}">Quản lý dịch vụ</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a href="{!! route('status.index') !!}" class="nav-link {{ Request::is('status*') ? 'active' : '' }}">
-                    <i class="fa fa-users"></i>
-                    <span>Quản Lý Trạng Thái</span>
-                </a>
-            </li>
+            @if(Auth::user()->role == App\Constants\UserConstant::ADMIN)
+                <li class="nav-item with-sub" {{ Request::is('category*')||Request::is('services*') ? 'active' : '' }}>
+                    <a class="nav-link" href="#"><i class="fas fa-newspaper"></i><span>Quản lý dịch vụ</span></a>
+                    <div class="sub-item">
+                        <ul>
+                            <li>
+                                <a href="{!! route('category.index') !!}">Danh mục dịch vụ</a>
+                            </li>
+                            <li>
+                                <a href="{!! route('services.index') !!}">Quản lý dịch vụ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a href="{!! route('status.index') !!}" class="nav-link {{ Request::is('status*') ? 'active' : '' }}">
+                        <i class="fa fa-users"></i>
+                        <span>Quản Lý Trạng Thái</span>
+                    </a>
+                </li>
+            @endif
             <li class="nav-item with-sub" {{ Request::is('statistics*') ? 'active' : '' }}>
                 <a class="nav-link" href="#"><i class="fas fa-newspaper"></i><span>Thống kê</span></a>
                 <div class="sub-item">
