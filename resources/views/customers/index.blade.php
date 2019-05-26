cdda37762e20afc5aba4cd5401d87c3a21817049
quang <quangphuong9685@gmail.com>
2019-05-26 19:03:36 +0700

    mau status

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('_style')
-    <link href="{{ asset('css/customer.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/customer.css') }}" rel="stylesheet"/>
 @endsection
 @section('content')
     <div class="col-md-12 col-lg-12">
@@ -14,8 +14,8 @@
                 <input class="form-control col-md-2 col-xs-12" name="search" placeholder="Search…" tabindex="1"
                        type="text" id="search">
                 <div class="scrollmenu">
-                    @foreach(@$statuses as $item)
-                        <button class="status">{{ $item->name }}</button>
+                    @foreach(@$statuses as $k => $item)
+                        <button class="status" style="background: {{$item->color ?:''}}">{{ $item->name }}</button>
                     @endforeach
                 </div>
                 <div>
@@ -27,6 +27,9 @@
                 </div>
             </div>
             <div id="registration-form">
+                <div class="load" style="text-align: center">
+                    <i class="fa fa-spinner fa-spin " style="font-size:46px"></i>
+                </div>
                 @include('customers.ajax')
             </div>
         </div>
@@ -34,7 +37,11 @@
 @endsection
 @section('_script')
     <script type="text/javascript">
+        $(document).ready(function () {
+            $('.load').hide();
+        })
         $(document).on('keyup', '#search', function (e) {
+            $('.load').show();
             e.preventDefault();
             var search = $(this).val();
             $.ajax({
@@ -42,19 +49,20 @@
                 method: "get",
                 data: {search: search}
             }).done(function (data) {
+                $('.load').hide();
                 $('#registration-form').html(data);
-
             });
         });
         $(document).on('click', '.status', function () {
+            $('.load').show();
             var status = $(this).html();
             $.ajax({
                 url: "{{ Url('customers/') }}",
                 method: "get",
                 data: {status: status}
             }).done(function (data) {
+                $('.load').hide();
                 $('#registration-form').html(data);
-
             });
         });
     </script>
