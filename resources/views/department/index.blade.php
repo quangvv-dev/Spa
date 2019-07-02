f1610974c6aec2f5aab934fa138cf2734625eb25
thanhtungs <thanhtunghb9xpro@gmail.com>
2019-07-02 13:02:34 +0700

    update

--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -11,7 +11,11 @@
                 <input class="form-control header-search col-md-2" name="search" placeholder="Search…" tabindex="1"
                        type="search">
                 <div class="col-md-2">
-                    {!! Form::select('type',$departments, null, array('class' => 'form-control header-search','placeholder'=>'Trực thuộc')) !!}
+                    {{--<select class="form-control header-search" name="parent_id">--}}
+                        {{--<option value="" disabled selected>Trực thuộc</option>--}}
+                        {{--{!! $departments !!}--}}
+                    {{--</select>--}}
+                    {!! Form::select('type',$category_pluck, null, array('class' => 'form-control header-search','placeholder'=>'Trực thuộc')) !!}
                 </div>
             </div>
             <div id="registration-form">
