79d62f2bf217935864dfb518dd37c497d0b59317
thanhtungs <thanhtunghb9spro@gmail.com>
2019-08-18 22:06:36 +0700

    fix search chart customer

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -27,17 +27,21 @@ class Category extends Model
 
         if (isset($input)) {
             $data = $data->when(isset($input['data_time']), function ($query) use ($input) {
-                $query->when($input['data_time'] == 'TODAY' ||
-                    $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                    $q->whereDate('created_at', getTime(($input['data_time'])));
-                })
-                    ->when($input['data_time'] == 'THIS_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'THIS_MONTH' ||
-                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                        $q->whereBetween('created_at', getTime(($input['data_time'])));
+                $query->whereHas('customers', function ($query) use ($input) {
+                    $query->whereHas('orders', function ($query) use ($input) {
+                        $query->when($input['data_time'] == 'TODAY' ||
+                            $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
+                            $q->whereDate('created_at', getTime(($input['data_time'])));
+                        })
+                            ->when($input['data_time'] == 'THIS_WEEK' ||
+                                $input['data_time'] == 'LAST_WEEK' ||
+                                $input['data_time'] == 'LAST_WEEK' ||
+                                $input['data_time'] == 'THIS_MONTH' ||
+                                $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                                $q->whereBetween('created_at', getTime(($input['data_time'])));
+                            });
                     });
+                });
             });
         }
 
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -24,17 +24,21 @@ class Status extends Model
 
         if (isset($input)) {
             $data = $data->when(isset($input['data_time']), function ($query) use ($input) {
-                $query->when($input['data_time'] == 'TODAY' ||
-                    $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                    $q->whereDate('created_at', getTime(($input['data_time'])));
-                })
-                    ->when($input['data_time'] == 'THIS_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'THIS_MONTH' ||
-                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                        $q->whereBetween('created_at', getTime(($input['data_time'])));
+                $query->whereHas('customers', function ($query) use ($input) {
+                    $query->whereHas('orders', function ($query) use ($input) {
+                        $query->when($input['data_time'] == 'TODAY' ||
+                            $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
+                            $q->whereDate('created_at', getTime(($input['data_time'])));
+                        })
+                            ->when($input['data_time'] == 'THIS_WEEK' ||
+                                $input['data_time'] == 'LAST_WEEK' ||
+                                $input['data_time'] == 'LAST_WEEK' ||
+                                $input['data_time'] == 'THIS_MONTH' ||
+                                $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                                $q->whereBetween('created_at', getTime(($input['data_time'])));
+                            });
                     });
+                });
             });
         }
 
@@ -52,17 +56,21 @@ class Status extends Model
 
         if (isset($input)) {
             $data = $data->when(isset($input['data_time']), function ($query) use ($input) {
-                $query->when($input['data_time'] == 'TODAY' ||
-                    $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                    $q->whereDate('created_at', getTime(($input['data_time'])));
-                })
-                    ->when($input['data_time'] == 'THIS_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'THIS_MONTH' ||
-                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                        $q->whereBetween('created_at', getTime(($input['data_time'])));
+                $query->whereHas('customers', function ($query) use ($input) {
+                    $query->whereHas('orders', function ($query) use ($input) {
+                        $query->when($input['data_time'] == 'TODAY' ||
+                            $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
+                            $q->whereDate('created_at', getTime(($input['data_time'])));
+                        })
+                        ->when($input['data_time'] == 'THIS_WEEK' ||
+                            $input['data_time'] == 'LAST_WEEK' ||
+                            $input['data_time'] == 'LAST_WEEK' ||
+                            $input['data_time'] == 'THIS_MONTH' ||
+                            $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                            $q->whereBetween('created_at', getTime(($input['data_time'])));
+                        });
                     });
+                });
             });
         }
 
@@ -78,17 +86,21 @@ class Status extends Model
 
         if (isset($input)) {
             $data = $data->when(isset($input['data_time']), function ($query) use ($input) {
-                $query->when($input['data_time'] == 'TODAY' ||
-                    $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                    $q->whereDate('created_at', getTime(($input['data_time'])));
-                })
-                    ->when($input['data_time'] == 'THIS_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'THIS_MONTH' ||
-                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                        $q->whereBetween('created_at', getTime(($input['data_time'])));
+                $query->whereHas('customers', function ($query) use ($input) {
+                    $query->whereHas('orders', function ($query) use ($input) {
+                        $query->when($input['data_time'] == 'TODAY' ||
+                            $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
+                            $q->whereDate('created_at', getTime(($input['data_time'])));
+                        })
+                        ->when($input['data_time'] == 'THIS_WEEK' ||
+                            $input['data_time'] == 'LAST_WEEK' ||
+                            $input['data_time'] == 'LAST_WEEK' ||
+                            $input['data_time'] == 'THIS_MONTH' ||
+                            $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                            $q->whereBetween('created_at', getTime(($input['data_time'])));
+                        });
                     });
+                });
             });
         }
 
