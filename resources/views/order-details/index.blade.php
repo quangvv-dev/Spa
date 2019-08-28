9d5feb1e0aec5f1a8cf66cdd2ffd10e235ba6ba4
thanhtungs <thanhtunghb9xpro@gmail.com>
2019-08-28 16:14:02 +0700

    fix search order

--- resources/views/order-details/index.blade.php
+++ resources/views/order-details/index.blade.php
@@ -56,10 +56,10 @@
                         </ul>
                     </div>
                     <div class="btn-group ml5">
-                        <button class="btn btn-default order_status" data-status="0">Đã hủy</button>
+                        <button class="btn btn-default order_status" data-status="">Đã hủy</button>
                     </div>
                     <div class="btn-group ml5">
-                        <button class="btn btn-default" id="payment_tab" data-status="1">Đã thu trong kỳ</button>
+                        <button class="btn btn-default bor-none" data-filter="advanced" id="payment_tab" >Đã thu trong kỳ</button>
                     </div>
                 </div>
             </div>
@@ -83,7 +83,6 @@
             const start_date = $('.filter_start_date').val();
             const end_date = $('.filter_end_date').val();
             const bor_none = $(target).find('.bor-none').data('filter');
-            const order_payment = $('#payment_tab').data('status');
             $(".other_time_panel").css({'display': 'none'});
             $("#boxSearch").css({'display': 'none'});
 
@@ -100,8 +99,7 @@
                     data_time: data_time,
                     start_date: start_date,
                     end_date: end_date,
-                    bor_none: bor_none,
-                    order_payment: order_payment
+                    bor_none: bor_none
                 }
             }).done(function (data) {
                 $('#registration-form').html(data);
@@ -131,6 +129,7 @@
         });
 
         $(document).on('click', '.order_status', function () {
+            $('.order_status').data('status', 0);
             const order_cancel = $('.order_status').data('status');
             $.ajax({
                 url: "{{ Url('list-orders/') }}",
@@ -141,36 +140,6 @@
             }).done(function (data) {
                 $('#registration-form').html(data);
             });
-        })
-
-        $(window).on('hashchange', function() {
-            const order_cancel = $('.order_status').data('status');
-            if (window.location.hash) {
-                var page = window.location.hash.replace('#', '');
-                if (page == Number.NaN || page <= 0) {
-                    return false;
-                } else {
-                    getPosts(page);
-                }
-            }
         });
-        $(document).ready(function() {
-            $(document).on('click', '.pagination a', function (e) {
-                e.preventDefault();
-                getPosts($(this).attr('href').split('page=')[1]);
-            });
-        });
-        function getPosts(page) {
-            $.ajax({
-                url : '?page=' + page,
-                dataType: 'json',
-                data: {
-                    order_cancel: $('.order_status').data('status')
-                }
-            }).done(function (data) {
-                $('#registration-form').html(data);
-                location.hash = page;
-            });
-        }
     </script>
 @endsection
