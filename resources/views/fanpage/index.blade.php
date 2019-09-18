2526c0421ac6bbf54c75ac31e655b2f240e5106e
quangvv-dev <quangphuong9685@gmail.com>
2019-09-18 15:57:09 +0700

    done time

--- resources/views/fanpage/index.blade.php
+++ resources/views/fanpage/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layout.app')
 @section('content')
+    <style>
+        .page-header {
+            display: none;
+        }
+    </style>
     <div class="col-md-12 col-lg-12">
         <div class="card">
             <div id="registration-form">
