ba4eb8630f35c8b12910da465658d26bafeb4e0b
quangvv-dev <quangphuong9685@gmail.com>
2019-09-18 17:39:05 +0700

    css ver 8.4

--- resources/views/fanpage/index.blade.php
+++ resources/views/fanpage/index.blade.php
@@ -18,7 +18,8 @@
     <div class="col-md-12 col-lg-12">
         <div class="card">
             <div id="registration-form">
-                <iframe id="frameDemo" width="100%" height="950px" src="https://smax.in/#/?returnUrl=%2Ffbpage"></iframe>
+                <iframe id="frameDemo" width="100%" height="950px"
+                        src="https://smax.in/#/?returnUrl=%2Ffbpage"></iframe>
             </div>
             <!-- table-responsive -->
         </div>
@@ -26,8 +27,9 @@
 @endsection
 @section('_script')
     <script>
-        $("#frameDemo").contents().find( ".menu2.hidden-sm.hidden-xs" ).css( "display-color", "none" );
-
+        $(window).bind("load", function () {
+            $("#frameDemo").contents().find(".menu2.hidden-sm.hidden-xs").css("display", "none");
+        });
         // $(window).load(function () {
         //     $('iframe').load(function () {
         //         console.log($('iframe').contents());
@@ -36,6 +38,5 @@
         //         $("#frameDemo").contents().find( ".menu2.hidden-sm.hidden-xs" ).css( "display-color", "none" );
         // });
         // });
-
     </script>
 @endsection
