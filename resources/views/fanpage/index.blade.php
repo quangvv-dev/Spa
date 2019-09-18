a7ee97dff93021da3530edeada18e184c7225ebc
quangvv-dev <quangphuong9685@gmail.com>
2019-09-18 17:29:57 +0700

    css ver 8.3

--- resources/views/fanpage/index.blade.php
+++ resources/views/fanpage/index.blade.php
@@ -18,7 +18,7 @@
     <div class="col-md-12 col-lg-12">
         <div class="card">
             <div id="registration-form">
-                <iframe width="100%" height="950px" src="https://smax.in/#/?returnUrl=%2Ffbpage"></iframe>
+                <iframe id="frameDemo" width="100%" height="950px" src="https://smax.in/#/?returnUrl=%2Ffbpage"></iframe>
             </div>
             <!-- table-responsive -->
         </div>
@@ -26,13 +26,16 @@
 @endsection
 @section('_script')
     <script>
-        $(window).load(function () {
-            $('iframe').load(function () {
-                console.log($('iframe').contents());
-                $('iframe').contents().find("head")
-                    .append($("<style type='text/css'>.menu2.hidden-sm.hidden-xs {display: none !important;}a.logo img {display: none !important;}  </style>"));
-            });
-        });
+        $("#frameDemo").contents().find( ".menu2.hidden-sm.hidden-xs" ).css( "display-color", "none" );
+
+        // $(window).load(function () {
+        //     $('iframe').load(function () {
+        //         console.log($('iframe').contents());
+        //     //     $('iframe').contents().find("head")
+        //     //         .append($("<style type='text/css'>.menu2.hidden-sm.hidden-xs {display: none !important;}a.logo img {display: none !important;}  </style>"));
+        //         $("#frameDemo").contents().find( ".menu2.hidden-sm.hidden-xs" ).css( "display-color", "none" );
+        // });
+        // });
 
     </script>
 @endsection
