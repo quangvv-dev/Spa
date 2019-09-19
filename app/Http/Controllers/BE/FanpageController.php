15e44d3a6cccbc5a0616f60807c1dae7c65c5b63
quangvv <quangphuong9685@gmail>
2019-09-19 22:33:48 +0700

    add customer

--- app/Http/Controllers/BE/FanpageController.php
+++ app/Http/Controllers/BE/FanpageController.php
@@ -2,13 +2,58 @@
 
 namespace App\Http\Controllers\BE;
 
+use App\Constants\StatusCode;
+use App\Constants\UserConstant;
+use App\Models\Category;
+use App\Models\Status;
+use App\Services\CustomerService;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class FanpageController extends Controller
 {
+
+    public function __construct(CustomerService $customerService)
+    {
+        $this->customerService = $customerService;
+        $status = Status::where('type', StatusCode::RELATIONSHIP)->pluck('name', 'id')->toArray();//mối quan hệ
+        $group = Category::pluck('name', 'id')->toArray();//nhóm KH
+        $source = Status::where('type', StatusCode::SOURCE_CUSTOMER)->pluck('name', 'id')->toArray();// nguồn KH
+        $branch = Status::where('type', StatusCode::BRANCH)->pluck('name', 'id')->toArray();// chi nhánh
+        $marketingUsers = User::where('role', UserConstant::MARKETING)->pluck('full_name', 'id')->toArray();
+        $telesales = User::where('role', UserConstant::TELESALES)->pluck('full_name', 'id')->toArray();
+        view()->share([
+            'status'         => $status,
+            'group'          => $group,
+            'source'         => $source,
+            'branch'         => $branch,
+            'telesales'      => $telesales,
+            'marketingUsers' => $marketingUsers,
+        ]);
+    }
+
     public function index()
     {
         return view('fanpage.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->except('group_id');
+        $input['mkt_id'] = $request->mkt_id;
+
+        $customer = $this->customerService->create($input);
+        $category = Category::find($request->group_id);
+        $customer->categories()->attach($category);
+
+        return back();
     }
 }
