5364f072630b2167f4702b664bd38846d6990fe6
tungtt <tungtt@ominext.com>
2019-10-10 09:57:38 +0700

    search report customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -164,13 +164,16 @@ class Customer extends Model
                     $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
                     $q->whereDate('created_at', getTime(($input['data_time'])));
                 })
-                    ->when($input['data_time'] == 'THIS_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'LAST_WEEK' ||
-                        $input['data_time'] == 'THIS_MONTH' ||
-                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                        $q->whereBetween('created_at', getTime(($input['data_time'])));
-                    });
+                ->when($input['data_time'] == 'THIS_WEEK' ||
+                    $input['data_time'] == 'LAST_WEEK' ||
+                    $input['data_time'] == 'LAST_WEEK' ||
+                    $input['data_time'] == 'THIS_MONTH' ||
+                    $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                    $q->whereBetween('created_at', getTime(($input['data_time'])));
+                });
+            })
+            ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
+                $q->whereBetween('created_at', [Functions::yearMonthDay($input['start_date'])." 00:00:00", Functions::yearMonthDay($input['end_date'])." 23:59:59"]);
             });
         }
 
