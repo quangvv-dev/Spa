3c8614be8076f3d24dd45ac2e073af848c9a48ee
tungtt <thanhtunghb9xpro@gmail.com>
2019-10-16 21:34:02 +0700

    update database

---
+++ database/migrations/2019_10_16_213149_create_user_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->comment('Người tham gia');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('task_id')->unsigned()->comment('Công việc');
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tasks');
+    }
+}
