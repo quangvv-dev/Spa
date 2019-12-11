de25db6b5f636623c88e564aedbd84e52dd8fc7e
tungtt <tungtt@ominext.com>
2019-12-11 14:31:07 +0700

    update layout

--- resources/views/group_comment/ajax.blade.php
+++ resources/views/group_comment/ajax.blade.php
@@ -1,14 +1,18 @@
-<div class="table-responsive">
+<div class="table-responsive" style="padding: 5px">
     <table class="table card-table table-vcenter text-nowrap table-primary">
         @if(count($docs))
             @foreach($docs as $item)
-                <div class="col-md-12 content_msg padding">
+                <div class="col-md-12 content_msg padding" style="border-top: 1px solid #c0c3c8;">
                     <div class="col row">
                         <div class="col-md-11"><p><a href="#" class="bold blue">{{isset($item->user)?$item->user->full_name:''}}</a>
                                 <span><i class="fa fa-clock"> {{$item->created_at}}</i></span></p>
                         </div>
                     </div>
-                    {!! $item->messages !!}
+                    <div class="col row">
+                        <div class="col-md-11">
+                            {!! $item->messages !!}
+                        </div>
+                    </div>
                     <br>
                 </div>
             @endforeach
