9cf084af23fcafc34388b7e597fb73fad8c97c41
Quangvv <quangvv@vinsofts.net>
2020-01-03 10:08:14 +0700

    change update_at order v2

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -86,17 +86,17 @@ class Order extends Model
             ->when(isset($input['data_time']), function ($query) use ($input) {
                 $query->when($input['data_time'] == 'TODAY' ||
                     $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                    $q->whereDate('created_at', getTime(($input['data_time'])));
+                    $q->whereDate('updated_at', getTime(($input['data_time'])));
                 })
                 ->when($input['data_time'] == 'THIS_WEEK' ||
                     $input['data_time'] == 'LAST_WEEK' ||
                     $input['data_time'] == 'THIS_MONTH' ||
                     $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                    $q->whereBetween('created_at', getTime(($input['data_time'])));
+                    $q->whereBetween('updated_at', getTime(($input['data_time'])));
                 });
             })
             ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
-                $q->whereBetween('created_at', [Functions::yearMonthDay($input['start_date'])." 00:00:00", Functions::yearMonthDay($input['end_date'])." 23:59:59"]);
+                $q->whereBetween('updated_at', [Functions::yearMonthDay($input['start_date'])." 00:00:00", Functions::yearMonthDay($input['end_date'])." 23:59:59"]);
             })
             ->when(isset($input['bor_none']), function ($query) use ($input) {
                 $query->when($input['bor_none'] == 'advanced', function ($q) use ($input) {
@@ -222,14 +222,14 @@ class Order extends Model
             $data = $data->when(isset($input['data_time']), function ($query) use ($input) {
                 $query->when($input['data_time'] == 'TODAY' ||
                     $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                        $q->whereDate('created_at', getTime(($input['data_time'])));
+                        $q->whereDate('updated_at', getTime(($input['data_time'])));
                 })
                 ->when($input['data_time'] == 'THIS_WEEK' ||
                     $input['data_time'] == 'LAST_WEEK' ||
                     $input['data_time'] == 'LAST_WEEK' ||
                     $input['data_time'] == 'THIS_MONTH' ||
                     $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                        $q->whereBetween('created_at', getTime(($input['data_time'])));
+                        $q->whereBetween('updated_at', getTime(($input['data_time'])));
                 });
             })
             ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
