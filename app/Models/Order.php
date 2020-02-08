94d595339dead63d1a9f6b85ba8c69789769a49c
quangvv <quangphuong9685@gmail>
2020-02-08 10:44:49 +0700

    done da thu trong ky

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -47,82 +47,14 @@ class Order extends Model
 
     public static function search($input)
     {
-        $data = self::with( 'orderDetails');
+        $data = self::with('orderDetails');
 
         if ($input) {
             $data = $data
-            ->when(isset($input['group']), function ($query) use ($input) {
-                $query->whereHas('customer', function ($q) use ($input) {
-                    $q->where('group_id', $input['group']);
-                });
-            })
-            ->when(isset($input['telesales']), function ($query) use ($input) {
-                $query->whereHas('customer', function ($q) use ($input) {
-                   $q->where('telesales_id', $input['telesales']);
-                });
-            })
-            ->when(isset($input['marketing']), function ($query) use ($input) {
-                $query->whereHas('customer', function ($q) use ($input) {
-                   $q->where('mkt_id', $input['marketing']);
-                });
-            })
-            ->when(isset($input['service']), function ($query) use ($input) {
-                $query->whereHas('orderDetails', function ($q) use ($input) {
-                   $q->where('booking_id', $input['service']);
-                });
-            })
-            ->when(isset($input['customer']), function ($query) use ($input) {
-                $query->whereHas('customer', function ($q) use ($input) {
-                   $q->where('full_name', 'like', '%'. $input['customer']. '%')
-                   ->orWhere('phone', 'like', '%'. $input['customer']. '%');
-                });
-            })
-            ->when(isset($input['payment_type']), function ($query) use ($input) {
-                $query->whereNotNull('payment_type')->where('payment_type', $input['payment_type']);
-            })
-            ->when(isset($input['order_type']), function ($query) use ($input) {
-                $query->where('type', $input['order_type']);
-            })
-            ->when(isset($input['data_time']), function ($query) use ($input) {
-                $query->when($input['data_time'] == 'TODAY' ||
-                    $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                    $q->whereDate('updated_at', getTime(($input['data_time'])));
-                })
-                ->when($input['data_time'] == 'THIS_WEEK' ||
-                    $input['data_time'] == 'LAST_WEEK' ||
-                    $input['data_time'] == 'THIS_MONTH' ||
-                    $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
-                    $q->whereBetween('updated_at', getTime(($input['data_time'])));
-                });
-            })
-            ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
-                $q->whereBetween('updated_at', [Functions::yearMonthDay($input['start_date'])." 00:00:00", Functions::yearMonthDay($input['end_date'])." 23:59:59"]);
-            })
-            ->when(isset($input['bor_none']), function ($query) use ($input) {
-                $query->when($input['bor_none'] == 'advanced', function ($q) use ($input) {
-                        $q->where('the_rest', '>', 0);
-                    })
-                    ->when($input['bor_none'] == 'paid', function ($q) use ($input) {
-                        $q->where('the_rest', 0);
+                ->when(isset($input['group']), function ($query) use ($input) {
+                    $query->whereHas('customer', function ($q) use ($input) {
+                        $q->where('group_id', $input['group']);
                     });
-            })
-            ->when(isset($input['order_cancel']), function ($query) use ($input) {
-                $query->onlyTrashed();
-            });
-        }
-
-        return $data->paginate(10);
-    }
-
-    public static function searchAll($input)
-    {
-        $data = self::with( 'orderDetails');
-
-        if ($input) {
-            $data = $data->when(isset($input['group']), function ($query) use ($input) {
-                    $customer = CustomerGroup::where('category_id', $input['group'])->pluck('customer_id')
-                    ->toArray();
-                    $query->whereIn('member_id', $customer);
                 })
                 ->when(isset($input['telesales']), function ($query) use ($input) {
                     $query->whereHas('customer', function ($q) use ($input) {
@@ -141,8 +73,79 @@ class Order extends Model
                 })
                 ->when(isset($input['customer']), function ($query) use ($input) {
                     $query->whereHas('customer', function ($q) use ($input) {
-                        $q->where('full_name', 'like', '%'. $input['customer']. '%')
-                            ->orWhere('phone', 'like', '%'. $input['customer']. '%');
+                        $q->where('full_name', 'like', '%' . $input['customer'] . '%')
+                            ->orWhere('phone', 'like', '%' . $input['customer'] . '%');
+                    });
+                })
+                ->when(isset($input['payment_type']), function ($query) use ($input) {
+                    $query->whereNotNull('payment_type')->where('payment_type', $input['payment_type']);
+                })
+                ->when(isset($input['order_type']), function ($query) use ($input) {
+                    $query->where('type', $input['order_type']);
+                })
+                ->when(isset($input['data_time']), function ($query) use ($input) {
+                    $query->when($input['data_time'] == 'TODAY' ||
+                        $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
+                        $q->whereDate('updated_at', getTime(($input['data_time'])));
+                    })
+                        ->when($input['data_time'] == 'THIS_WEEK' ||
+                            $input['data_time'] == 'LAST_WEEK' ||
+                            $input['data_time'] == 'THIS_MONTH' ||
+                            $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                            $q->whereBetween('updated_at', getTime(($input['data_time'])));
+                        });
+                })
+                ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
+                    $q->whereBetween('updated_at', [
+                        Functions::yearMonthDay($input['start_date']) . " 00:00:00",
+                        Functions::yearMonthDay($input['end_date']) . " 23:59:59",
+                    ]);
+                })
+                ->when(isset($input['bor_none']), function ($query) use ($input) {
+                    $query->when($input['bor_none'] == 'advanced', function ($q) use ($input) {
+                        $q->where('the_rest', '>', 0);
+                    })
+                        ->when($input['bor_none'] == 'paid', function ($q) use ($input) {
+                            $q->where('the_rest', 0);
+                        });
+                })
+                ->when(isset($input['order_cancel']), function ($query) use ($input) {
+                    $query->onlyTrashed();
+                });
+        }
+
+        return $data->paginate(10);
+    }
+
+    public static function searchAll($input)
+    {
+        $data = self::with('orderDetails');
+
+        if ($input) {
+            $data = $data->when(isset($input['group']), function ($query) use ($input) {
+                $customer = CustomerGroup::where('category_id', $input['group'])->pluck('customer_id')
+                    ->toArray();
+                $query->whereIn('member_id', $customer);
+            })
+                ->when(isset($input['telesales']), function ($query) use ($input) {
+                    $query->whereHas('customer', function ($q) use ($input) {
+                        $q->where('telesales_id', $input['telesales']);
+                    });
+                })
+                ->when(isset($input['marketing']), function ($query) use ($input) {
+                    $query->whereHas('customer', function ($q) use ($input) {
+                        $q->where('mkt_id', $input['marketing']);
+                    });
+                })
+                ->when(isset($input['service']), function ($query) use ($input) {
+                    $query->whereHas('orderDetails', function ($q) use ($input) {
+                        $q->where('booking_id', $input['service']);
+                    });
+                })
+                ->when(isset($input['customer']), function ($query) use ($input) {
+                    $query->whereHas('customer', function ($q) use ($input) {
+                        $q->where('full_name', 'like', '%' . $input['customer'] . '%')
+                            ->orWhere('phone', 'like', '%' . $input['customer'] . '%');
                     });
                 })
                 ->when(isset($input['payment_type']), function ($query) use ($input) {
@@ -164,24 +167,29 @@ class Order extends Model
                         });
                 })
                 ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
-                    $q->whereBetween('created_at', [Functions::yearMonthDay($input['start_date'])." 00:00:00", Functions::yearMonthDay($input['end_date'])." 23:59:59"]);
+                    $q->whereBetween('created_at', [
+                        Functions::yearMonthDay($input['start_date']) . " 00:00:00",
+                        Functions::yearMonthDay($input['end_date']) . " 23:59:59",
+                    ]);
                 })
                 ->when(isset($input['bor_none']), function ($query) use ($input) {
                     $query->when($input['bor_none'] == 'unpaid', function ($q) use ($input) {
                         $q->where('gross_revenue', 0);
                     })
-                    ->when($input['bor_none'] == 'paid', function ($q) use ($input) {
-                        $q->where('the_rest', 0);
-                    })
-                    ->when($input['bor_none'] == 'payment', function ($q) use ($input) {
-                        $q->where('gross_revenue', '>', 0);
-                    });
+                        ->when($input['bor_none'] == 'paid', function ($q) use ($input) {
+                            $q->where('the_rest', 0);
+                        })
+                        ->when($input['bor_none'] == 'payment', function ($q) use ($input) {
+                            $detail = PaymentHistory::whereDate('created_at', '=', date('Y-m-d'))->pluck('order_id')
+                                ->toArray();
+                            $q->whereIn('id', $detail);
+                        });
                 })
                 ->when(isset($input['order_cancel']), function ($query) use ($input) {
                     $query->onlyTrashed();
                 });
             if (isset($input['phone'])) {
-                $customer = Customer::where('phone', 'like', '%'. $input['phone'] . '%')->pluck('id');
+                $customer = Customer::where('phone', 'like', '%' . $input['phone'] . '%')->pluck('id');
 
                 $data = $data->where('member_id', $customer);
             }
@@ -216,7 +224,7 @@ class Order extends Model
     {
         parent::boot(); // TODO: Change the autogenerated stub
 
-        static::deleting(function($order) {
+        static::deleting(function ($order) {
             $order->orderDetails()->delete();
         });
     }
@@ -229,19 +237,22 @@ class Order extends Model
             $data = $data->when(isset($input['data_time']), function ($query) use ($input) {
                 $query->when($input['data_time'] == 'TODAY' ||
                     $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
-                        $q->whereDate('updated_at', getTime(($input['data_time'])));
+                    $q->whereDate('updated_at', getTime(($input['data_time'])));
                 })
-                ->when($input['data_time'] == 'THIS_WEEK' ||
-                    $input['data_time'] == 'LAST_WEEK' ||
-                    $input['data_time'] == 'LAST_WEEK' ||
-                    $input['data_time'] == 'THIS_MONTH' ||
-                    $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                    ->when($input['data_time'] == 'THIS_WEEK' ||
+                        $input['data_time'] == 'LAST_WEEK' ||
+                        $input['data_time'] == 'LAST_WEEK' ||
+                        $input['data_time'] == 'THIS_MONTH' ||
+                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
                         $q->whereBetween('updated_at', getTime(($input['data_time'])));
-                });
+                    });
             })
-            ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
-                $q->whereBetween('updated_at', [Functions::yearMonthDay($input['start_date'])." 00:00:00", Functions::yearMonthDay($input['end_date'])." 23:59:59"]);
-            });
+                ->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
+                    $q->whereBetween('updated_at', [
+                        Functions::yearMonthDay($input['start_date']) . " 00:00:00",
+                        Functions::yearMonthDay($input['end_date']) . " 23:59:59",
+                    ]);
+                });
         }
 
         $data = $data->get();
