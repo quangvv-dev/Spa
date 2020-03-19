a707d98ead4b54ffee0f12fdbadd79cfbc008b8f
Beat Quang <quangphuong9685@gmail.com>
2020-03-19 21:54:35 +0700

    sms ban 3 nguoi

--- app/Console/Commands/SmsRevenue.php
+++ app/Console/Commands/SmsRevenue.php
@@ -42,20 +42,22 @@ class  SmsRevenue extends Command
      */
     public function handle()
     {
-        Functions::sendSmsBK();
-//        $input = [
-//            'start_date' => Carbon::now()->format('Y-m-d'),
-//            'end_date'   => Carbon::now()->format('Y-m-d'),
-//        ];
-//        $detail = PaymentHistory::search($input);
-//        $total = $detail->sum('price');
-//        $orders = Order::whereBetween('created_at', [
-//            Functions::yearMonthDay(Carbon::now()->format('Y-m-d')) . " 00:00:00",
-//            Functions::yearMonthDay(Carbon::now()->format('Y-m-d')) . " 23:59:59",
-//        ])->with('orderDetails');
-//        $all_total = $orders->sum('all_total');
-//        $grossRevenue = $orders->sum('gross_revenue');
-//        $text = request()->getHttpHost() . ' trong ngay ' . Carbon::now()->format('d/m/Y') . ' co DS: '.@number_format($all_total).' DT: '.@number_format($grossRevenue).' DTTK: ' . @number_format($total) . ' VND';
-//        Functions::sendSms('0334299996', $text);
+//        Functions::sendSmsBK();
+        $input = [
+            'start_date' => Carbon::now()->format('Y-m-d'),
+            'end_date'   => Carbon::now()->format('Y-m-d'),
+        ];
+        $detail = PaymentHistory::search($input);
+        $total = $detail->sum('price');
+        $orders = Order::whereBetween('created_at', [
+            Functions::yearMonthDay(Carbon::now()->format('Y-m-d')) . " 00:00:00",
+            Functions::yearMonthDay(Carbon::now()->format('Y-m-d')) . " 23:59:59",
+        ])->with('orderDetails');
+        $all_total = $orders->sum('all_total');
+        $grossRevenue = $orders->sum('gross_revenue');
+        $text = request()->getHttpHost() . ' trong ngay ' . Carbon::now()->format('d/m/Y') . ' co DS: ' . @number_format($all_total) . ' DT: ' . @number_format($grossRevenue) . ' DTTK: ' . @number_format($total) . ' VND';
+        Functions::sendSms('0986898662', $text);//chithuan
+        Functions::sendSms('0989996738', $text);//Athien
+        Functions::sendSms('0334299996', $text);//Sy
     }
 }
