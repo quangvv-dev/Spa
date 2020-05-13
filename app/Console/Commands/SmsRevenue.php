ede00de1eebe04b74a4c6083b5afbc672769b823
Beat Quang <quangphuong9685@gmail.com>
2020-05-13 14:52:10 +0700

    test sms v5.1

--- app/Console/Commands/SmsRevenue.php
+++ app/Console/Commands/SmsRevenue.php
@@ -57,7 +57,7 @@ class  SmsRevenue extends Command
         $text = request()->getHttpHost() . ' trong ngay ' . Carbon::now()->format('d/m/Y') . ' co DS: ' . @number_format($all_total) . ' DT: ' . @number_format($grossRevenue) . ' DTTK: ' . @number_format($total) . ' VND';
 //        Functions::sendSmsBK('84986898662', $text);//chithuan
 //        Functions::sendSmsBK('84989996738', $text);//Athien
-        $time = '13-05-2020 14:55';;
+        $time = '13-05-2020 14:55';
         Functions::sendSmsV3('0353997108', $text, $time);
 //        Functions::sendSmsBK('84334299996', $text);//Sy
     }
