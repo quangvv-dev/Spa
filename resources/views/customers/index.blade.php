bcbb92baf4917712bc00565a3c80b49dcd72ad1a
Beat Quang <quangphuong9685@gmail.com>
2020-05-26 10:33:07 +0700

    trao doi danh

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -127,11 +127,11 @@
                                  src="{{asset('default/no-image.png')}}" style="border-radius:100%">
 
                             <div class="mt10 pb10" style="height:86px">
-                            <div class="col-md-10 info-avatar padding5 last_contacthover box_last bold">
+                            <div class="col-md-10 info-avatar padding5 last_contacthover box_last">
                             <p><i class="fa fa-user mr5" style="color: black;"></i> ` + data.customer.full_name + `
                                 <i class="fa orange fa-star" aria-hidden="true" style="color: orange;"></i>
                             </p>
-                            <p class="mt10"><i class="fa fa-phone mr10" style="color: black;" aria-hidden="true"></i><a class="__clickToCall blue bold" data-contact-id="5678"
+                            <p class="mt10"><i class="fa fa-phone mr10" style="color: black;" aria-hidden="true"></i><a class="__clickToCall blue" data-contact-id="5678"
                                                           rel="tooltip" data-original-title="Click để gọi"
                                                           data-placement="right" data-flag="1"
                                                           data-type="crm"> ` + data.customer.phone + `</a></p>
