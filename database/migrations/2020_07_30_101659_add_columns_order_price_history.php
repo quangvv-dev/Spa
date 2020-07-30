d85e78f6b18028f8377286fd9f77e5248168f0fa
Beat Quang <quangphuong9685@gmail.com>
2020-07-30 11:34:17 +0700

    menu wallet v3

--- database/migrations/2020_07_30_101659_add_columns_order_price_history.php
+++ database/migrations/2020_07_30_101659_add_columns_order_price_history.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsOrderPrice extends Migration
+class AddColumnsOrderPriceHistory extends Migration
 {
     /**
      * Run the migrations.
