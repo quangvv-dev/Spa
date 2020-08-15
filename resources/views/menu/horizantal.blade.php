3f594298458975933664a2601e55174c6fdfca8a
Beat Quang <quangphuong9685@gmail.com>
2020-08-15 10:21:07 +0700

    fix menu

--- resources/views/menu/horizantal.blade.php
+++ resources/views/menu/horizantal.blade.php
@@ -2,12 +2,12 @@
 <div class="ren-navbar fixed-header" id="headerMenuCollapse">
     <div class="container">
         <ul class="nav">
-            <li class="nav-item" {{ Request::is('statistics*')? 'active' : '' }}>
-                <a class="nav-link" href="{!! url('statistics') !!}"><i
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('statistics')? 'active' : '' }}" href="{!! url('statistics') !!}"><i
                         class="fas fa-newspaper"></i><span>Chi nhánh</span></a>
             </li>
-            <li class="nav-item" {{ Request::is('statistics-branch*')? 'active' : '' }}>
-                <a class="nav-link" href="{!! url('statistics-branch') !!}"><i
+            <li class="nav-item" >
+                <a class="nav-link {{ Request::is('statistics-branch*')? 'active' : '' }}" href="{!! url('statistics-branch') !!}"><i
                         class="fas fa-newspaper"></i><span>Toàn hệ thống</span></a>
             </li>
         </ul>
