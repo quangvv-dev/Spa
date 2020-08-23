798b7b93ecd48fd0cc9a346a6c10a109bf650e07
Beat Quang <quangphuong9685@gmail.com>
2020-08-23 16:07:50 +0700

    chi tiet sales

--- app/Http/Controllers/API/StatisticController.php
+++ app/Http/Controllers/API/StatisticController.php
@@ -9,6 +9,7 @@ use App\Models\OrderDetail;
 use App\Models\PaymentHistory;
 use App\Models\Status;
 use App\User;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
 use App\Models\Services;
 use App\Models\Category;
@@ -191,9 +192,56 @@ class StatisticController extends BaseApiController
             $item->payment_new = $order_new->sum('gross_revenue') + $order_old->sum('gross_revenue');//da thu trong ky
             return $item;
         })->sortByDesc('gross_revenue');
-//        $data = [
-//            'users' => $users,
-//        ];
+
+        return $this->responseApi(200, 'SUCCESS', $users);
+    }
+
+    /**
+     * Sale with branch
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function saleWithBranch(Request $request)
+    {
+        if (empty($request->data_time)) {
+            $request->merge(['data_time' => 'THIS_MONTH']);
+        }
+
+        $users = User::whereIn('role', [UserConstant::TELESALES, UserConstant::WAITER])->get()->map(function ($item) use ($request) {
+            $data_new = Customer::select('id')->where('telesales_id', $item->id)->whereBetween('created_at', getTime($request->data_time));
+            $data_old = Customer::select('id')->where('telesales_id', $item->id)->where('created_at', '<', getTime($request->data_time)[0]);
+            $data = Customer::select('id')->where('telesales_id', $item->id);
+
+            $order = Order::whereBetween('created_at', getTime($request->data_time))->whereIn('member_id', $data->pluck('id')->toArray())->with('orderDetails');
+            $order_new = Order::whereIn('member_id', $data_new->pluck('id')->toArray())->whereBetween('created_at', getTime($request->data_time))->with('orderDetails');//doanh so
+            $order_old = Order::whereBetween('created_at', getTime($request->data_time))->whereIn('member_id', $data_old->pluck('id')->toArray())->with('orderDetails');
+
+            $item->comment_new = GroupComment::select('id')->whereIn('customer_id', $data_new->pluck('id')->toArray())->whereBetween('created_at', getTime($request->data_time))->get()->count();// trao doi moi
+            $item->comment_old = GroupComment::select('id')->whereIn('customer_id', $data_old->pluck('id')->toArray())->whereBetween('created_at', getTime($request->data_time))->get()->count(); // trao doi cu
+
+            $item->schedules_new = Schedule::select('id')->where('creator_id', $item->id)->whereIn('user_id', $data_new->pluck('id')->toArray())->whereBetween('created_at', getTime($request->data_time))->get()->count();//lich hen
+            $item->schedules_old = Schedule::select('id')->where('creator_id', $item->id)->whereIn('user_id', $data_old->pluck('id')->toArray())->whereBetween('date', getTime($request->data_time))->get()->count();//lich hen
+
+            $item->customer_new = $data_new->get()->count();
+            $item->order_new = $order_new->count();
+            $item->order_old = $order_old->count();
+            $item->revenue_new = $order_new->sum('all_total');
+            $item->revenue_old = $order->sum('all_total') - $order_new->sum('all_total');
+            $item->payment_revenue = $order->sum('gross_revenue');
+            $item->payment_new = $order_new->sum('gross_revenue');//da thu trong ky
+            $item->payment_old = $order->sum('gross_revenue') - $order_new->sum('gross_revenue'); //da thu trong ky
+//            $item->payment_rest = PaymentHistory::whereBetween('payment_date', getTime($request->data_time))->whereIn('order_id', $order_old->pluck('id')->toArray())->sum('price');//da thu trong ky thu thêm
+
+            $item->revenue_total = $order->sum('all_total');
+            return $item;
+        })->sortByDesc('revenue_total');
+
+        $data = [
+            'allTotal' => $users->sum('revenue_total'),
+            'grossRevenue' => $users->sum('payment_revenue'),
+            'user' => $users,
+        ];
         return $this->responseApi(200, 'SUCCESS', $users);
 
     }
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::post('/check-unique-code-orders', 'BE\OrderController@checkUniqueCode');
 Route::get('/statistics', 'API\StatisticController@index');
 Route::get('/statistics-all', 'API\StatisticController@getAllBranch');
 Route::get('/sales', 'API\StatisticController@sales');
+Route::get('/sales-with-branch', 'API\StatisticController@saleWithBranch');
