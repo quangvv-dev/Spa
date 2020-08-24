5fa31e1ad3284e9b82ea192fc61dff1e251b7f46
Beat Quang <quangphuong9685@gmail.com>
2020-08-24 16:00:47 +0700

    thong ke doanh thu toan he thong v2

--- resources/views/statistics/ajax_branch.blade.php
+++ resources/views/statistics/ajax_branch.blade.php
@@ -10,20 +10,20 @@
     }
 </style>
 @php
-    $all_total = [];
-    $payment = [];
-    $gross_revenue = [];
-    $orders = [];
-    $customers = [];
+    $all_total = 0;
+    $payment = 0;
+    $gross_revenue = 0;
+    $orders = 0;
+    $customers = 0;
     $revenue_month = [];
     $total_month = [];
     $users = [];
         foreach($response as $k =>$item){
-        $all_total[]=(int)$item->all_total;
-        $payment[]=(int)$item->payment;
-        $orders[]=(int)$item->orders;
-        $customers[]=(int)$item->customers;
-        $gross_revenue[]=(int)$item->gross_revenue;
+        $all_total      += (int)$item->all_total;
+        $payment        += (int)$item->payment;
+        $orders         += (int)$item->orders;
+        $customers      += (int)$item->customers;
+        $gross_revenue  += (int)$item->gross_revenue;
             foreach ((array)$item->revenue_month as $key => $value){
                 if (array_key_exists($key, $revenue_month) ==false){
                     $revenue_month[$key] = $value;
@@ -50,7 +50,7 @@
             <div class="card-body text-center">
                 <div class="h5">Tổng số SĐT</div>
                 <div class="h3 font-weight-bold mb-4 font-30"><span
-                        class="">{{number_format(array_sum($customers))}}</span></div>
+                        class="">{{number_format($customers)}}</span></div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-gradient-orange" style="width: 100%"></div>
                 </div>
@@ -62,7 +62,7 @@
             <div class="card-body text-center">
                 <div class="h5">Tổng số đơn hàng</div>
                 <div class="h3 font-weight-bold mb-4 font-30"><span
-                        class="">{{number_format(array_sum($orders))}}</span>
+                        class="">{{number_format($orders)}}</span>
                 </div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-gradient-orange" style="width: 100%"></div>
@@ -75,7 +75,7 @@
             <div class="card-body text-center">
                 <div class="h5">Tổng doanh số</div>
                 <div class="h3 font-weight-bold mb-4 font-30"><span
-                        class="">{{number_format(array_sum($all_total))}}</span></div>
+                        class="">{{number_format($all_total)}}</span></div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-gradient-orange" style="width: 100%"></div>
                 </div>
@@ -87,7 +87,7 @@
             <div class="card-body text-center  bg-gradient-indigo text-white">
                 <div class="h5">Tổng doanh thu</div>
                 <div class="h3 font-weight-bold mb-4 font-30"><span
-                        class="">{{number_format(array_sum($gross_revenue))}}</span></div>
+                        class="">{{number_format($gross_revenue)}}</span></div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-gradient-orange" style="width: 100%"></div>
                 </div>
@@ -98,7 +98,7 @@
         <div class="card  overflow-hidden">
             <div class="card-body text-center bg-gradient-indigo text-white">
                 <div class="h5">Đã thu trong kỳ</div>
-                <div class="h3 font-weight-bold mb-4 font-30 ">{{number_format(array_sum($payment))}}</div>
+                <div class="h3 font-weight-bold mb-4 font-30 ">{{number_format($payment)}}</div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-gradient-orange" style="width: 100%"></div>
                 </div>
@@ -126,15 +126,14 @@
     function drawBasic() {
         var data = google.visualization.arrayToDataTable([
                 @if(count($response))
-            {{--{{dd(count($response))}}--}}
+                {{--{{dd(count($response))}}--}}
             ['Năm', 'Doanh số', {role: 'annotation'}, 'Doanh thu', {role: 'annotation'}, 'Đã thu trong kỳ', {role: 'annotation'}],
-            @foreach($response as $k =>$item1)
+                @foreach($response as $k =>$item1)
             ['{{$k}}',{{$item1->all_total}}, '{{number_format($item1->all_total)}}',{{$item1->gross_revenue}}, '{{number_format($item1->gross_revenue)}}',{{$item1->payment}} , '{{number_format($item1->payment)}}'],
                 @endforeach
                 @else
             ['Năm', 0, '#fffff', '0%'],
             @endif
-
 
         ]);
 
