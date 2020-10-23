4fe71e196e7fff2f01de25bd79e3c8c098240b19
quangphuong9685 <quangphuong9685@gmail.com>
2020-10-23 10:12:33 +0700

    first promotion

---
+++ app/Http/Controllers/BE/PromotionController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers\BE;
+
+use App\Constants\StatusCode;
+use App\Models\Category;
+use App\Models\Services as Service;
+use App\Models\Status;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Helpers\Functions;
+use Illuminate\Support\Facades\Response;
+
+class PromotionController extends Controller
+{
+    /**
+     * ServiceController constructor.
+     */
+    public function __construct()
+    {
+        $status = Status::where('type', StatusCode::RELATIONSHIP)->pluck('name', 'id');
+        view()->share([
+            'status' => $status,
+        ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $docs = Service::where('type', StatusCode::SERVICE)->orderBy('id', 'desc');
+        if ($request->search) {
+            $docs = $docs->where('name', 'like', '%' . $request->search . '%')
+                ->orwhere('code', 'like', '%' . $request->search . '%')
+                ->orwhere('trademark', 'like', '%' . $request->search . '%')
+                ->orwhere('enable', 'like', '%' . $request->search . '%');
+        }
+        $docs = $docs->paginate(10);
+
+        $title = 'Quản lý dịch vụ';
+        if ($request->ajax()) {
+            return Response::json(view('service.ajax', compact('docs', 'title'))->render());
+        }
+        return view('service.index', compact('title', 'docs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $title = 'Thêm dịch vụ';
+        return view('service._form', compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->except('img_file');
+        if ($request->hasFile('img_file')) {
+            $imgs = [];
+            if ($request->img_file) {
+                foreach (@$request->img_file as $k => $v) {
+                    $img = Functions::uploadImage($v, 'services');
+                    $imgs[] = $img;
+                }
+            }
+            $input['images'] = @json_encode($imgs);
+        }
+        $input['price_buy'] = $request->price_buy ? str_replace(',', '', $request->price_buy) : 0;
+        $input['price_sell'] = $request->price_sell ? str_replace(',', '', $request->price_sell) : 0;
+        $input['promotion_price'] = $request->promotion_price ? str_replace(',', '', $request->promotion_price) : 0;
+        $data = Service::create($input);
+        $data->update([
+            'code' => $data->id,
+        ]);
+        return redirect(route('services.create'))->with('status', 'Tạo dịch vụ thành công');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Service $service)
+    {
+        $doc = $service;
+        $title = 'Cập nhật dịch vụ';
+        return view('service._form', compact('title', 'doc'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Service $service)
+    {
+        $request->merge([
+            'price_buy' => $request->price_buy ? str_replace(',', '', $request->price_buy) : 0,
+            'price_sell' => $request->price_sell ? str_replace(',', '', $request->price_sell) : 0,
+            'promotion_price' => $request->promotion_price ? str_replace(',', '', $request->promotion_price) : 0,
+        ]);
+        $image = Functions::checkUploadImage($request, $service, 'services');
+        $service->update($request->except('img_file', 'image'));
+        return redirect('services/' . $service->id . '/edit')->with('status', 'Cập nhật dịch vụ thành công');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Service $service)
+    {
+        if ($service->images) {
+            foreach ($service->images as $k => $v) {
+                Functions::unlinkUpload('services', @$v);
+            }
+        }
+        $service->delete();
+    }
+}
---
+++ resources/views/promotions/_form.blade.php
@@ -0,0 +1,61 @@
+@extends('layout.app')
+@section('content')
+    <div class="col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">{{$title}}</h3></br>
+            </div>
+
+            @if (isset($doc))
+                {!! Form::model($doc, array('url' => url('category/'.$doc->id), 'method' => 'put', 'files'=> true,'id'=>'fvalidate')) !!}
+            @else
+                {!! Form::open(array('url' => route('category.store'), 'method' => 'post', 'files'=> true,'id'=>'fvalidate')) !!}
+            @endif
+            <div class="col row">
+                <div class="col-xs-12 col-md-6">
+                    <div class="form-group required {{ $errors->has('name') ? 'has-error' : '' }}">
+                        {!! Form::label('name', 'Tên danh mục', array('class' => ' required')) !!}
+                        {!! Form::text('name',null, array('class' => 'form-control', 'required' => true)) !!}
+                        <span class="help-block">{{ $errors->first('name', ':message') }}</span>
+                    </div>
+                </div>
+                @if (isset($doc))
+                    <div class="col-xs-12 col-md-6">
+                        <div class="form-group required {{ $errors->has('name') ? 'has-error' : '' }}">
+                            {!! Form::label('code', 'Mã nhóm', array('class' => ' required')) !!}
+                            {!! Form::text('code',null, array('class' => 'form-control', 'required' => true,'readonly'=>true)) !!}
+                            <span class="help-block">{{ $errors->first('name', ':message') }}</span>
+                        </div>
+                    </div>
+                @endif
+                <div class="col-xs-12 col-md-6">
+                    <div class="form-group {{ $errors->has('parent_id') ? 'has-error' : '' }}">
+                        {!! Form::label('parent_id','Danh mục cha', array('class' => 'required')) !!}
+                        {!! Form::select('parent_id',$category_pluck, @$doc->parent_id, array('class' => 'form-control select2')) !!}
+                        <span class="help-block">{{ $errors->first('parent_id', ':message') }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col bot">
+                <button type="submit" class="btn btn-success">Lưu</button>
+                <a href="{{route('category.index')}}" class="btn btn-danger">Về danh sách</a>
+            </div>
+            {{ Form::close() }}
+
+        </div>
+    </div>
+@endsection
+@section('_script')
+    <script>
+        $(document).ready(function () {
+            $('form#fvalidate').validate({
+                rules: {
+                    name: 'required',
+                },
+                messages: {
+                    name: "vui lòng nhâp tên danh mục",
+                }
+            });
+        })
+    </script>
+@endsection
