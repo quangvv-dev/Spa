1c568b6ac8af3763cf424b99f30f3c05543a5a0e
Beat Quang <quangphuong9685@gmail.com>
2020-12-05 18:02:59 +0700

    trademark

---
+++ app/Http/Controllers/BE/TrademarkController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\BE;
+
+use App\Models\Trademark;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TrademarkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Trademark $trademark
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trademark $trademark)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Trademark $trademark
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trademark $trademark)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Models\Trademark $trademark
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trademark $trademark)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Trademark $trademark
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trademark $trademark)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_12_05_162129_create_trademarks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrademarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trademarks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trademarks');
+    }
+}
