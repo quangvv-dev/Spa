800e5ba854c3339020978806fd669333f23480a0
Beat Quang <quangphuong9685@gmail.com>
2021-03-22 15:51:26 +0700

    create migration

---
+++ database/migrations/2021_03_22_154458_create_branchs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branchs', function (Blueprint $table) {
+                $table->Increments('id');
+                $table->string('name')->comment('Chi nhánh');
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('branchs', function (Blueprint $table) {
+            //
+        });
+    }
+}
