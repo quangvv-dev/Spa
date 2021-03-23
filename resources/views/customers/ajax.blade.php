3e39cd722859bf2ffe8fdc310c590952ef9c3fde
Beat Quang <quangphuong9685@gmail.com>
2021-03-23 18:01:09 +0700

    fix done css

--- resources/views/customers/ajax.blade.php
+++ resources/views/customers/ajax.blade.php
@@ -129,7 +129,7 @@
         </div>
     </div>
 </div>
-<div class="table-responsive" style="position: relative;font-size: 12px">
+<div class="table-responsive fixed-scrollbar" style="position: relative;font-size: 12px">
     <table class="table card-table table-vcenter text-nowrap table-primary" style="width: 100%">
         <thead class="bg-primary text-white">
         <tr>
@@ -294,16 +294,79 @@
 @include('customers.modal_view')
 <!-- table-responsive -->
 <script>
-    // $('.table-responsive .table-primary').floatThead({
-    //     top: 196,
-    //     scrollContainer: function ($table) {
-    //         return $table.closest('');
-    //     },
-    //     position: 'absolute'
-    // });
-    // $('.table-ajax .table-primary').floatThead({
-    //     top: 196,
-    //     position: 'absolute'
-    // });
+    $(function ($) {
+        var scrollbar = $('<div id="fixed-scrollbar"><div></div></div>').appendTo($(document.body));
+        scrollbar.hide().css({
+            overflowX: 'auto',
+            position: 'fixed',
+            width: '100%',
+            bottom: 0
+        });
+        var fakecontent = scrollbar.find('div');
+
+        function top(e) {
+            return e.offset().top;
+        }
+
+        function bottom(e) {
+            return e.offset().top + e.height();
+        }
+
+        var active = $([]);
+        function find_active() {
+            scrollbar.show();
+            var active = $([]);
+            $('.fixed-scrollbar').each(function () {
+                if (top($(this)) < top(scrollbar) && bottom($(this)) > bottom(scrollbar)) {
+                    fakecontent.width($(this).get(0).scrollWidth);
+                    fakecontent.height(1);
+                    active = $(this);
+                }
+            });
+            fit(active);
+            return active;
+        }
+
+        function fit(active) {
+            if (!active.length) return scrollbar.hide();
+            scrollbar.css({ left: active.offset().left, width: active.width() });
+            fakecontent.width($(this).get(0).scrollWidth);
+            fakecontent.height(1);
+            delete lastScroll;
+        }
+
+        function onscroll() {
+            var oldactive = active;
+            active = find_active();
+            if (oldactive.not(active).length) {
+                oldactive.unbind('scroll', update);
+            }
+            if (active.not(oldactive).length) {
+                active.scroll(update);
+            }
+            update();
+        }
+
+        var lastScroll;
+        function scroll() {
+            if (!active.length) return;
+            if (scrollbar.scrollLeft() === lastScroll) return;
+            lastScroll = scrollbar.scrollLeft();
+            active.scrollLeft(lastScroll);
+        }
+
+        function update() {
+            if (!active.length) return;
+            if (active.scrollLeft() === lastScroll) return;
+            lastScroll = active.scrollLeft();
+            scrollbar.scrollLeft(lastScroll);
+        }
+
+        scrollbar.scroll(scroll);
+
+        onscroll();
+        $(window).scroll(onscroll);
+        $(window).resize(onscroll);
+    });
     $('html, body').animate({scrollTop: '0px'}, 300);
 </script>
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -57,21 +57,22 @@
         .table-responsive {
             overflow-y: scroll;
         }
-
+        @media only screen and (max-width: 1921px) {
+            .table-responsive {
+                max-height: 80vh;
+            }
+        }
+        @media only screen and (max-width: 1441px) {
+            .table-responsive {
+                max-height: 75vh;
+            }
+        }
         @media only screen and (max-width: 1367px) {
             .table-responsive {
                 max-height: 65vh;
             }
         }
-        @media only screen and (max-width: 1441px) {
-            .table-responsive {
-                max-height: 78vh;
-            }
-        }@media only screen and (max-width: 1921px) {
-            .table-responsive {
-                max-height: 88vh;
-            }
-        }
+
     </style>
     {{--<script src="https://unpkg.com/floatthead@2.1.4/dist/jquery.floatThead.min.js"></script>--}}
     <!-- end anheasy -->
@@ -1068,5 +1069,81 @@
                 });
             });
         }());
+    </script>
+    <script>
+        $(function ($) {
+            var scrollbar = $('<div id="fixed-scrollbar"><div></div></div>').appendTo($(document.body));
+            scrollbar.hide().css({
+                overflowX: 'auto',
+                position: 'fixed',
+                width: '100%',
+                bottom: 0
+            });
+            var fakecontent = scrollbar.find('div');
+
+            function top(e) {
+                return e.offset().top;
+            }
+
+            function bottom(e) {
+                return e.offset().top + e.height();
+            }
+
+            var active = $([]);
+            function find_active() {
+                scrollbar.show();
+                var active = $([]);
+                $('.fixed-scrollbar').each(function () {
+                    if (top($(this)) < top(scrollbar) && bottom($(this)) > bottom(scrollbar)) {
+                        fakecontent.width($(this).get(0).scrollWidth);
+                        fakecontent.height(1);
+                        active = $(this);
+                    }
+                });
+                fit(active);
+                return active;
+            }
+
+            function fit(active) {
+                if (!active.length) return scrollbar.hide();
+                scrollbar.css({ left: active.offset().left, width: active.width() });
+                fakecontent.width($(this).get(0).scrollWidth);
+                fakecontent.height(1);
+                delete lastScroll;
+            }
+
+            function onscroll() {
+                var oldactive = active;
+                active = find_active();
+                if (oldactive.not(active).length) {
+                    oldactive.unbind('scroll', update);
+                }
+                if (active.not(oldactive).length) {
+                    active.scroll(update);
+                }
+                update();
+            }
+
+            var lastScroll;
+            function scroll() {
+                if (!active.length) return;
+                if (scrollbar.scrollLeft() === lastScroll) return;
+                lastScroll = scrollbar.scrollLeft();
+                active.scrollLeft(lastScroll);
+            }
+
+            function update() {
+                if (!active.length) return;
+                if (active.scrollLeft() === lastScroll) return;
+                lastScroll = active.scrollLeft();
+                scrollbar.scrollLeft(lastScroll);
+            }
+
+            scrollbar.scroll(scroll);
+
+            onscroll();
+            $(window).scroll(onscroll);
+            $(window).resize(onscroll);
+        });
     </script>
 @endsection
