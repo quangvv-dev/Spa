fbf44cd350059384963f95df77ebc87c88ad1b45
Beat Quang <quangphuong9685@gmail.com>
2021-04-17 18:08:39 +0700

    tang font size v4

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -54,8 +54,7 @@
         font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
     }
     tr td {
-        font-size: 14px;
-        font-weight: 500;
+        font-family: 'Poppins', sans-serif;
     }
 
 </style>
