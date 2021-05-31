b0e9b5878719a7b7e883ca658dd3bd72b8539052
Beat Quang <quangphuong9685@gmail.com>
2021-05-31 15:59:55 +0700

    fix urrl v2

--- resources/views/call_center/stream.blade.php
+++ resources/views/call_center/stream.blade.php
@@ -2,6 +2,16 @@
 <head>
     <meta name="viewport" content="width=device-width">
 </head>
+<style>
+    body{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /* align-self: center; */
+        overflow: hidden;
+        height: 100vh;
+    }
+</style>
 <body>
 <video controls="" autoplay="" name="media">
     <source
