94c2e436e2c6bb7325a0b647fd7066f328a735e0
quangvv-dev <quangphuong9685@gmail.com>
2021-12-01 17:11:50 +0700

    fix bxh sale

--- resources/views/report_products/ajax_sale.blade.php
+++ resources/views/report_products/ajax_sale.blade.php
@@ -171,7 +171,10 @@
             <td class="text-center bold">{{!empty($payment_old)&& !empty($order_old)?number_format($payment_old/$order_old):0}}</td>
             <td class="bold">{{number_format($allTotal)}}</td>
             <td class="bold">{{number_format($all_payment - $grossRevenue)}}</td>
-            <td class="bold">{{number_format($all_payment)}}da thu trong ky</td>
+
+            <td class="bold">{{number_format($all_payment - $payment_new - $payment_old)}} thu nợ</td>
+
+            <td class="bold">{{number_format($all_payment)}}</td>
         </tr>
 
         </tbody>
