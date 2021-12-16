a1c4bd57ffb99c719595324d6eb88d842ceba3e3
quangvv-dev <quangphuong9685@gmail.com>
2021-12-16 16:02:03 +0700

    sms multiple

---
+++ resources/views/sms/sms_multiple.blade.php
@@ -0,0 +1,40 @@
+<style>
+    label.required:after {
+        content: " *";
+        color: red;
+    }
+
+    ul#textcomplete-dropdown-2 {
+        max-height: 300px;
+        overflow: auto;
+    }
+</style>
+<div class="col-md-12 col-lg-12">
+    <div class="card">
+        {!! Form::open(array('url' => route('sms.saveSchedules'), 'method' => 'post', 'files'=> true,'id'=>'fvalidate','class'=>'sent-sms')) !!}
+        <div class="col row">
+            <div class="col-md-12 row">
+                <div class="col-6 form-group">
+                    {!! Form::label('list_phone', 'Danh sách SĐT', array('class' => ' required')) !!}
+                    {!! Form::textArea('list_phone', null, array('class' => 'form-control autocomplete-textarea','rows'=>7)) !!}
+                    <span
+                        class="help-block">Có thể nhập nhiều số điện thoại cách nhau bằng dấu ";" hoặc xuống dòng</span>
+                </div>
+
+                <div class="col-6 form-group">
+                    {!! Form::label('content', 'Nội dung tin nhắn KH đặt lịch', array('class' => ' required')) !!}
+                    {!! Form::textArea('content', setting('content_multiple_sms'), array('class' => 'form-control','required'=>true,'rows'=>7)) !!}
+                    <span class="help-block">Lưu ý nội dung tin nhắn không dấu</span>
+                </div>
+            </div>
+            <div class="col bot" style="margin-top: 5px">
+                <button type="submit" class="btn btn-success">Lưu</button>
+            </div>
+        </div>
+
+        {{ Form::close() }}
+    </div>
+</div>
+@section('_script')
+
+@endsection
