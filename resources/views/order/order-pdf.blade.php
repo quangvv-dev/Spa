bc1a8febc8ada9089fe2a368df54bdd331321ebb
quangvv-dev <quangphuong9685@gmail.com>
2021-12-29 17:38:17 +0700

    payment

--- resources/views/order/order-pdf.blade.php
+++ resources/views/order/order-pdf.blade.php
@@ -52,86 +52,91 @@
 
 <div class="invoice">
     <div class="row">
-        <div class="col-xs-12">
-        <table class="table mt0">
-            <tbody>
-            <tr>
-                <td colspan="2" class="text-center"><h3>{{!empty(setting('title_website'))?setting('title_website'):'Hệ Thống Royal Spa'}}</h3><br></td>
-            </tr>
-            <tr>
-                <td colspan="2" class="text-center"><b >ĐƠN HÀNG BÁN</b></td>
-            </tr>
-            <tr class="font12">
-                <td class="padding5">Ngày : {{ date('d-m-Y', strtotime($payment->payment_date)) }}</td>
-                <td class="padding5">HĐ: {{ $order->code }}</td>
-            </tr>
-            <tr class="font12">
-                <td class="padding5">Khách hàng : {{ @$order->customer->full_name }}</td>
-                <td class="padding5">SĐT: {{ @$order->customer->phone }}</td>
-            </tr>
-            </tbody>
-        </table>
-        <table class="table table-bordered mt10">
-            <tbody>
-            <tr class="bold b-gray">
-                <td class="padding5"><strong>Dịch vụ</strong></td>
-                <td class="padding5"><strong>Đơn giá</strong></td>
-                <td class="padding5"><strong>SL</strong></td>
-                <td class="padding5"><strong>T.Tiền</strong></td>
-            </tr>
-            @foreach($order->orderDetails as $key => $orderDetail)
-                <tr class="font12">
-                    <td class="tl position">{{ $orderDetail->service->name }}</td>
-                    <td class="tc">{{ number_format($orderDetail->price) }}</td>
-                    <td class="tc">{{ $orderDetail->quantity }}</td>
-                    <td class="tr">{{ number_format($orderDetail->total_price) }}</td>
-                </tr>
-            @endforeach
-            <tr>
-                <td class="bold" colspan="3"><strong>Giảm giá</strong></td>
-                <td class="tr bold"><strong>{{ @number_format($order->discount) }}</strong></td>
-            </tr>
-            <tr>
-                <td class="font-bold" colspan="3"><strong>Tổng Tiền</strong></td>
-                <td class="tr bold"><strong>{{ @number_format($order->all_total) }}</strong></td>
-            </tr>
-            <tr>
-                <td class="font-bold" colspan="4"><strong>Khách T.Toán</strong></td>
-            </tr>
-            <tr>
-                <td class="font12" colspan="3">{{$payment->payment_type==1?'Tiền mặt':($payment->payment_type==2?'Thẻ':($payment->payment_type==4?'Chuyển khoản':'Điểm'))}}</td>
-                <td class="tr bold"><strong>{{ @number_format($payment->price) }}</strong></td>
-            </tr>
-            <tr>
-                <td class="font-bold" colspan="3"><strong>Còn lại</strong></td>
-                <td class="tr bold"><strong>{{ number_format($order->the_rest) }}</strong></td>
-            </tr>
-            </tbody>
-            <tfoot>
-            <tr>
-                <td style="padding: 0px !important;" class="font-bold" colspan="2"><strong>Khách hàng</strong></td>
-                <td style="padding: 0px !important;" class="tr bold" colspan="2"><strong>Nhân viên</strong></td>
-            </tr>
-            <tr class="font12">
-                <td style="padding: 0px !important;" class="font-bold" colspan="2"><i>(Chữ ký)</i></td>
-                <td style="padding: 0px !important;" class="tr bold" colspan="2"><i>(Chữ ký)</i></td>
-            </tr>
-            <tr style="margin-top: 10px">
-                <td><p></p><p></p></td>
-            </tr>
-            {{--<tr>--}}
-                {{--<td style="border-bottom:dotted 1px !important;" colspan="4"></td>--}}
-            {{--</tr>--}}
-            <tr>
-                <td colspan="4">
-                    <div class="text-center"><b>CÁM ƠN QUÝ KHÁCH VÀ HẸN GẶP LẠI</b></div>
-                    <div class="text-center font12">{{'Hotline: '.@$order->branch->phone.' Địa chỉ: '.@$order->branch->address}}</div>
-                    {{--<div class="text-center font12">Website: thammyroyal.com</div>--}}
-                </td>
-            </tr>
-            </tfoot>
-        </table>
-        </div>
+        @if(isset($payment))
+            <div class="col-xs-12">
+                <table class="table mt0">
+                    <tbody>
+                    <tr>
+                        <td colspan="2" class="text-center"><h3>{{!empty(setting('title_website'))?setting('title_website'):'Hệ Thống Royal Spa'}}</h3><br></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="text-center"><b >ĐƠN HÀNG BÁN</b></td>
+                    </tr>
+                    <tr class="font12">
+                        <td class="padding5">Ngày : {{isset($payment)? date('d-m-Y', strtotime($payment->payment_date)):'' }}</td>
+                        <td class="padding5">HĐ: {{ $order->code }}</td>
+                    </tr>
+                    <tr class="font12">
+                        <td class="padding5">Khách hàng : {{ @$order->customer->full_name }}</td>
+                        <td class="padding5">SĐT: {{ @$order->customer->phone }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table class="table table-bordered mt10">
+                    <tbody>
+                    <tr class="bold b-gray">
+                        <td class="padding5"><strong>Dịch vụ</strong></td>
+                        <td class="padding5"><strong>Đơn giá</strong></td>
+                        <td class="padding5"><strong>SL</strong></td>
+                        <td class="padding5"><strong>T.Tiền</strong></td>
+                    </tr>
+                    @foreach($order->orderDetails as $key => $orderDetail)
+                        <tr class="font12">
+                            <td class="tl position">{{ $orderDetail->service->name }}</td>
+                            <td class="tc">{{ number_format($orderDetail->price) }}</td>
+                            <td class="tc">{{ $orderDetail->quantity }}</td>
+                            <td class="tr">{{ number_format($orderDetail->total_price) }}</td>
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <td class="bold" colspan="3"><strong>Giảm giá</strong></td>
+                        <td class="tr bold"><strong>{{ @number_format($order->discount) }}</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold" colspan="3"><strong>Tổng Tiền</strong></td>
+                        <td class="tr bold"><strong>{{ @number_format($order->all_total) }}</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold" colspan="4"><strong>Khách T.Toán</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="font12" colspan="3">{{$payment->payment_type==1?'Tiền mặt':($payment->payment_type==2?'Thẻ':($payment->payment_type==4?'Chuyển khoản':'Điểm'))}}</td>
+                        <td class="tr bold"><strong>{{ @number_format($payment->price) }}</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold" colspan="3"><strong>Còn lại</strong></td>
+                        <td class="tr bold"><strong>{{ number_format($order->the_rest) }}</strong></td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td style="padding: 0px !important;" class="font-bold" colspan="2"><strong>Khách hàng</strong></td>
+                        <td style="padding: 0px !important;" class="tr bold" colspan="2"><strong>Nhân viên</strong></td>
+                    </tr>
+                    <tr class="font12">
+                        <td style="padding: 0px !important;" class="font-bold" colspan="2"><i>(Chữ ký)</i></td>
+                        <td style="padding: 0px !important;" class="tr bold" colspan="2"><i>(Chữ ký)</i></td>
+                    </tr>
+                    <tr style="margin-top: 10px">
+                        <td><p></p><p></p></td>
+                    </tr>
+                    {{--<tr>--}}
+                    {{--<td style="border-bottom:dotted 1px !important;" colspan="4"></td>--}}
+                    {{--</tr>--}}
+                    <tr>
+                        <td colspan="4">
+                            <div class="text-center"><b>CÁM ƠN QUÝ KHÁCH VÀ HẸN GẶP LẠI</b></div>
+                            <div class="text-center font12">{{'Hotline: '.@$order->branch->phone.' Địa chỉ: '.@$order->branch->address}}</div>
+                            {{--<div class="text-center font12">Website: thammyroyal.com</div>--}}
+                        </td>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+            @else
+            Chưa thanh toán đơn hàng
+        @endif
+
     </div>
 </div>
 <script>
