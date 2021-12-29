5f24fe610766c123bb929074bb18ce49ae89f6fc
quangvv-dev <quangphuong9685@gmail.com>
2021-12-29 17:29:09 +0700

    vi done all

--- resources/views/order/payment-history.blade.php
+++ resources/views/order/payment-history.blade.php
@@ -1,5 +1,5 @@
 <h3
-        class="bor-bot uppercase font12 mg0 bold padding5">Lịch sử thanh toán</h3></div>
+    class="bor-bot uppercase font12 mg0 bold padding5">Lịch sử thanh toán</h3></div>
 <div class="col-md-12 no-padd">
     <table class="table table-bordered">
         <thead class="b-gray">
@@ -11,17 +11,20 @@
         </tr>
         </thead>
         <tbody id="payment-history" style="font-size: 12px">
-        @foreach($payment as $item)
-            <tr>
-                <td class="fz-12">{{ date('d-m-Y', strtotime($item->payment_date)) }}</td>
-                <td class="fz-12">{{ number_format($item->price) }}</td>
-                <td class="fz-12">{{ $item->description }}</td>
-                <td class="fz-12">
-                    <a title="Xóa" class="btn delete" href="javascript:void(0)"
-                       data-url="{{ url('payment-wallet/' . $item->id) }}"><i class="fas fa-trash-alt"></i></a>
-                </td>
-            </tr>
-        @endforeach
+        @if(count($payment))
+            @foreach($payment as $item)
+                <tr>
+                    <td class="fz-12">{{ date('d-m-Y', strtotime($item->payment_date)) }}</td>
+                    <td class="fz-12">{{ number_format($item->price) }}</td>
+                    <td class="fz-12">{{ $item->description }}</td>
+                    <td class="fz-12">
+                        <a title="Xóa" class="btn delete" href="javascript:void(0)"
+                           data-url="{{ url('payment-wallet/' . $item->id) }}"><i class="fas fa-trash-alt"></i></a>
+                    </td>
+                </tr>
+            @endforeach
+        @endif
+
         </tbody>
     </table>
 </div>
