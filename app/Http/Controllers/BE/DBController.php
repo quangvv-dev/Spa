f7fd7eba486ea7f465c5b38fb86b89b6aa818f93
quangvv-dev <quangphuong9685@gmail.com>
2022-01-05 14:54:59 +0700

    fix order

--- app/Http/Controllers/BE/DBController.php
+++ app/Http/Controllers/BE/DBController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\BE;
 
 use App\Http\Controllers\Controller;
-use App\Models\Customer;
 use App\Models\Order;
-use App\Models\Commission;
 
 class DBController extends Controller
 {
     public function index()
     {
-//        Customer::whereNotIn('telesales_id', [68, 98])->update(['telesales_id' => 91]);
-        return 1;
+        $orders = Order::select('member_id')->whereBetween('created_at', ["2022-01-01 00:00:00", "2022-01-30 23:59:59"])
+            ->whereHas('customer', function ($qr) {
+                $qr->where('old_customer', 1);
+            })->groupBy('member_id')
+            ->pluck('member_id')->toArray();
+
+        return $orders;
     }
 }
