12b0626f399c8e9107c2f442f538f9eda98a8516
Adamtech Admin <quangphuong9685@gmail.com>
2022-03-03 10:32:39 +0700

    them gender

--- resources/views/customers/dropdownFilter.blade.php
+++ resources/views/customers/dropdownFilter.blade.php
@@ -1,12 +1,21 @@
 <div class="card-header isShowView" style="display:none;">
-    <div class="card-header fix-header bottom-card">
+    <div class="fix-header bottom-card">
         <div class="row">
-            <select name="source" class="form-control source">
-                <option value="">Nguồn</option>
-                @foreach($source as $k=> $item)
-                    <option value="{{$k}}">{{ $item}}</option>
-                @endforeach
-            </select>
+            <div class="col-md-6 col-xs-12">
+                <select name="source" class="form-control source">
+                    <option value="">Nguồn</option>
+                    @foreach($source as $k=> $item)
+                        <option value="{{$k}}">{{ $item}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-6 col-xs-12">
+                <select name="gender" class="form-control gender">
+                    <option value="">Giới tính</option>
+                    <option value="0">Nữ</option>
+                    <option value="1">Nam</option>
+                </select>
+            </div>
         </div>
     </div>
 </div>
