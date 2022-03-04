7c3d45606077555c4eb83fd4c5fcb374e1f76d72
Tien Nguyen <tiennm298@gmail.com>
2022-03-04 14:29:00 +0700

    orderby customer

--- app/Http/Controllers/BE/CustomerController.php
+++ app/Http/Controllers/BE/CustomerController.php
@@ -115,7 +115,7 @@ class CustomerController extends Controller
         $page = $request->page;
         $customers = Customer::search($input);
 
-        $customers = $customers->take(StatusCode::PAGINATE_1000)->get();
+        $customers = $customers->take(StatusCode::PAGINATE_1000)->orderByDesc('updated_at')->get();
         if (isset($input['limit'])) {
             $customers = Functions::customPaginate($customers, $page, $input['limit']);
         } else {
