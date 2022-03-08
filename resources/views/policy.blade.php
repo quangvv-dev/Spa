c44073c073c88e17ff61d3209e1d18112efc7448
quangvv-dev <quangphuong9685@gmail.com>
2022-03-08 15:07:08 +0700

    policy

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Công Ty Cổ Phần Tập Đoàn Adam Group</title>
+    <link rel="apple-touch-icon" href="{{asset('backend/app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon"
+          href="{{asset('default/logo.png')}}">
+</head>
+<body>
+<div class="container">
+    <img width="70" height="70  " src="https://thuongmai.adamtech.vn/default/logo.png" alt="anh logo">
+    <p style="text-align: justify;">Chào mừng bạn đến website Thammyroyal.adamtech.vn. Website này và những dịch vụ trên website được xây dựng và thuộc sở hữu của Công ty Cổ phần Tập Đoàn Adam Group. Việc bạn sử dụng website và các dịch vụ của website đồng nghĩa với việc bạn đã đồng ý với những thoả thuận dưới đây. Nếu bạn không đồng ý, xin vui lòng không sử dụng website và những dịch vụ của chúng tôi. Chúng tôi có thể sẽ thay đổi hoặc điều chỉnh Chính sách bảo mật. Bạn nên thường xuyên xem Chính sách bảo mật mỗi khi vào website. Nếu bạn tiếp tục sử dụng website và những dịch vụ sau khi đã điều chỉnh tức là bạn đã đồng ý với Chính sách bảo mật mới của chúng tôi.</p>
+    <h3 style="text-align: justify;"><span style="font-size: 12pt;"><strong>1. THÔNG TIN CHÚNG TÔI THU THẬP</strong></span></h3>
+    <p style="text-align: justify;">Chúng tôi sử dụng tin nhắn facebook để lấy ra những thông tin sau:</p>
+    <ul style="text-align: justify;">
+        <li>Tên tài khoản facebook</li>
+        <li>Số điện thoại có trong nội dung tin nhắn</li>
+        <li>Số điện thoại có trong nội dung bình luận</li>
+    </ul>
+    <p style="text-align: justify;">Thông tin được ghi nhận với tất cả khách hàng gửi thông tin tại Thammyroyal.adamtech.vn trên phạm vi toàn cầu.</p>
+    <h3 style="text-align: justify;"><span style="font-size: 12pt;"><strong>2. SỬ DỤNG THÔNG TIN CỦA BẠN</strong></span></h3>
+    <h3 style="text-align: justify;"><span style="font-size: 12pt;"><strong>Mục đích sử dụng thông tin</strong></span></h3>
+    <p style="text-align: justify;">Mục tiêu của AdamGroup là mang lại cho khách hàng những dịch vụ và sản phẩm phù hợp nhất, tạo nên hiệu quả kinh tế, đột phá doanh thu cho các doanh nghiệp. Với mục đích này, chúng tôi sử dụng các thông tin thu thập được để cung cấp dịch vụ và sự hỗ trợ tốt nhất cho khách hàng như:</p>
+    <ul style="text-align: justify;">
+        <li style="text-align: justify;">Liên hệ với khách hàng để cung cấp thông tin về các sản phẩm, dịch vụ</li>
+        <li style="text-align: justify;">Giải đáp thắc mắc của khách hàng về thông tin của các sản phẩm, dịch vụ</li>
+        <li>Gọi điên thông báo các chương trình, sự kiện, cảnh báo bảo mật của chúng tôi</li>
+        <li>Đo lường và cải thiện các dịch vụ của chúng tôi</li>
+    </ul>
+    <h3 style="text-align: justify;"><span style="font-size: 12pt;"><strong>Quy định sử dụng thông tin</strong></span></h3>
+    <ul style="text-align: justify;">
+        <li style="text-align: justify;">Mọi thông tin cá nhân của khách hàng được hoàn toàn bảo mật, được quản lý bởi giải pháp quản trị trong hệ thống và các tiêu chuẩn bảo mật thông tin.</li>
+        <li style="text-align: justify;">Khách hàng được toàn quyền thay đổi thông tin cá nhân của bản thân, theo quy định của Thammyroyal.adamtech.vn và quy định của pháp luật.</li>
+        <li style="text-align: justify;">Khách hàng có quyền yêu cầu nhân viên kĩ thuật hỗ trợ thay đổi thông tin cá nhân của tài khoản theo quy định.</li>
+        <li style="text-align: justify;">Mọi nhân viên hỗ trợ khách hàng không được sử dụng thông tin khách hàng dưới mọi hình thức, trừ trường hợp khách hàng có yêu cầu.</li>
+    </ul>
+    <h3><strong><span style="font-size: 12pt;">Phạm vi sử dụng thông tin</span></strong></h3>
+    <p style="text-align: justify;">Thông tin khách hàng được lưu trữ tại hệ thống của Thammyroyal.adamtech.vn tối thiểu là 03 năm, nếu trong vòng 03 năm khách hàng không phát sinh đăng ký thông tin mới thì Thammyroyal.adamtech.vn sẽ xóa các thông tin khách hàng.</p>
+    <p style="text-align: justify;">Thông tin khách hàng được lưu trữ tại hệ thống của Thammyroyal.adamtech.vn tối thiểu là 03 năm, nếu trong vòng 03 năm khách hàng không phát sinh đăng ký thông tin mới thì Thammyroyal.adamtech.vn sẽ xóa các thông tin khách hàng.</p>
+    <h3 style="text-align: justify;"><span style="font-size: 12pt;">3. ĐƠN VỊ THU THẬP VÀ SỬ DỤNG THÔNG TIN</span></h3>
+    <p style="text-align: justify;">Thông tin cá nhân mà bạn cung cấp được thu thập và sử dụng bởi:</p>
+    <p style="text-align: justify;">CÔNG TY CỔ PHẦN TẬP ĐOÀN ADAM GROUP<br>
+        Địa chỉ: Số 29 ngõ 151B Đường Thái Hà, Phường Láng Hạ Thanh Xuân Hà Nội<br>
+        Điện thoại: 04.6259.3148 (Thời gian hỗ trợ từ 09:00 am đến 6:00 pm)</p>
+    <h3 style="text-align: justify;"><span style="font-size: 12pt;">4. CAM KẾT BẢO MẬT THÔNG TIN</span></h3>
+    <p style="text-align: justify;">Chúng tôi cam kết bảo đảm an toàn thông tin cho khách hàng khi gửi thông tin cá nhân tới Thammyroyal.adamtech.vn. Không trao đổi mua bán thông tin khách hàng vì mục đích thương mại. Mọi sự chia sẻ và sử dụng thông tin khách hàng, chúng tôi cam kết thực hiện đúng theo Chính sách bảo mật này. Adam Group không cung cấp thông tin cá nhân của bạn cho bất kỳ bên thứ ba nào. Tuy nhiên, chúng tôi có thể tiết lộ thông tin cá nhân của bạn trong một số trường hợp dưới đây:</p>
+    <ul style="text-align: justify;">
+        <li>Chúng tôi được bạn đồng ý tiết lộ những thông tin này</li>
+        <li>Bên thứ ba mà bạn ủy quyền hoặc cho phép có yêu cầu chúng tôi cung cấp thông tin cá nhân của bạn</li>
+        <li>Theo yêu cầu pháp lý hay từ một cơ quan chính phủ hoặc nếu chúng tôi tin rằng hành động đó là cần thiết nhằm tuân theo các yêu cầu pháp lý hoặc chiếu theo luật pháp.</li>
+        <li>Bảo vệ quyền, lợi ích, tài sản, sự an toàn của một ai khác</li>
+        <li>Cho các bên thứ ba khác mà chúng tôi có liên doanh, liên kết để cung cấp các dịch vụ trên Thammyroyal.adamtech.vn hoặc các dịch vụ mới có liên quan đến Thammyroyal.adamtech.vn</li>
+    </ul>
+    <p style="text-align: justify;">Ngoài những trường hợp nêu trên, chúng tôi sẽ không công bố thông tin cá nhân của bạn cho bên thứ ba nào khác trừ khi chúng tôi hoàn toàn tin rằng, sự công bố này là cần thiết nhằm ngăn chặn những thiệt hại vật chất hoặc tài chính do các yếu tố có dấu hiệu phạm pháp có thể gây ra.</p>
+    <p style="text-align: justify;">Chính sách bảo mật thông tin chỉ áp dụng đối với những thông tin mà quý khách hàng gửi trên website chính thức Thammyroyal.adamtech.vn. Mọi thông tin quý khách đăng ký tại những website hoặc những địa chỉ khác đều không thuộc phạm vi hiệu lực của Chính sách bảo mật thông tin này. Chúng tôi luôn nhấn mạnh khẳng định rằng: Chúng tôi rất coi trọng quyền lợi được bảo mật thông tin của quý khách hàng. Nếu như quý khách hàng có bất cứ thắc mắc hay góp ý nào liên quan đến chính sách bảo mật của Thammyroyal.adamtech.vn, hãy liên hệ ngay với chúng tôi.</p>
+    <p style="text-align: justify;">Mọi thắc mắc quý khách hàng có thể liên hệ trực tiếp đến công ty để được giải đáp:</p>
+    <p style="text-align: justify;">CÔNG TY CỔ PHẦN TẬP ĐOÀN ADAM GROUP<br>
+        Địa chỉ: Số 29 ngõ 151B Đường Thái Hà, Phường Láng Hạ Thanh Xuân Hà Nội<br>
+        Điện thoại: 04.6259.3148 (Thời gian hỗ trợ từ 09:00 am đến 6:00 pm)</p>
+    <h4 style="text-align: justify;"><span style="font-size: 10pt;"><strong>BQT Thammyroyal.adamtech.vn</strong></span></h4>
+</div>
+
+</body>
+</html>
+
+
