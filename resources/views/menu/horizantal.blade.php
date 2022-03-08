f0bc20d311aff42624ea8dc2f34eb0230e6a43cd
Tien Nguyen <tiennm298@gmail.com>
2022-03-08 09:39:56 +0700

    menu fanpage post

--- resources/views/menu/horizantal.blade.php
+++ resources/views/menu/horizantal.blade.php
@@ -73,8 +73,9 @@
                         @if($roleGlobal->permission('fanpage'))
                             <li><a href="{{route('marketing.fanpage.index')}}">Fanpage </a></li>
                         @endif
+                            <li><a href="{{route('marketing.fanpage-post.index')}}">Fanpage Post</a></li>
 
-                        {{--<li><a href="/marketing/fanpage-post">Danh sách bài post</a></li>--}}
+                            {{--<li><a href="/marketing/fanpage-post">Danh sách bài post</a></li>--}}
                         {{--<li><a href="/marketing/seeding-number">Kho số seeding</a></li>--}}
                         {{--<li><a href="/marketing/source-fb">Nguồn dữ liệu</a></li>--}}
 
