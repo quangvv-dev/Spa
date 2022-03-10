e78d5f36d3f1e89985d9e80caec9d6e476ccb9d0
Tien Nguyen <tiennm298@gmail.com>
2022-03-10 16:05:41 +0700

    hoi thoai fb

--- resources/views/message_fb/js_chat_app.blade.php
+++ resources/views/message_fb/js_chat_app.blade.php
@@ -6,14 +6,15 @@
         let token = $('.chat-token').val();
         let url = 'https://graph.facebook.com/v10.0/'+page_id+'/conversations?fields=messages{message,from,created_time}&user_id='+sender_id+'&access_token='+token;
         let html = '';
-        $.ajax({
-            url: url,
-            success: function (res) {
-                let data = res.data[0].messages.data.reverse();
-                if(data && data.length > 0){
-                    data.forEach(function (item) {
-                        if(item.from.id == sender_id){ //khách
-                            html+= `
+        if(page_id && sender_id && token){
+            $.ajax({
+                url: url,
+                success: function (res) {
+                    let data = res.data[0].messages.data.reverse();
+                    if(data && data.length > 0){
+                        data.forEach(function (item) {
+                            if(item.from.id == sender_id){ //khách
+                                html+= `
                                 <div class="media media-chat">
                                     <img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png"
                                                                    alt="...">
@@ -25,25 +26,26 @@
                                     </div>
                                 </div>
                             `
-                        } else {
-                            html += `
+                            } else {
+                                html += `
                                 <div class="media media-chat media-chat-reverse">
                                     <div class="media-body">
                                         <p>`+item.message+`</p>
                                     </div>
                                 </div>
                             `
-                        }
-                    })
-                    html = `
+                            }
+                        })
+                        html = `
                         <div class="ps-container ps-theme-default ps-active-y"
                          style="overflow-y: scroll !important; height:400px !important;">
                         `+html+`
                         </div>
                     `
-                    $('.chatContent').html(html)
+                        $('.chatContent').html(html)
+                    }
                 }
-            }
-        })
+            })
+        }
     })
 </script>
