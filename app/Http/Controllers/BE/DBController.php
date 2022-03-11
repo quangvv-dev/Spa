b22e6ef3acae5d50a392489b6e74c41b41366e27
quangvv-dev <quangphuong9685@gmail.com>
2022-03-11 16:42:39 +0700

    database v2

--- app/Http/Controllers/BE/DBController.php
+++ app/Http/Controllers/BE/DBController.php
@@ -14,9 +14,9 @@ class DBController extends Controller
     public function index(Request $request)
     {
         for ($i = 1; $i < 17; $i++) {
-            $status = Functions::getStatusWithCode('nguoi_mua_hang');
+            $status = Functions::getStatusWithCode('khach_hang');
             $order = Order::select('member_id', \DB::raw('COUNT(member_id) AS total'))->where('branch_id', $i)
-                ->groupBy('member_id')->havingRaw('total = 1')->pluck('member_id');
+                ->groupBy('member_id')->havingRaw('total > 1')->pluck('member_id');
             Customer::whereIn('id', $order)->update(['status_id' => $status]);
         }
         return "OKIE";
