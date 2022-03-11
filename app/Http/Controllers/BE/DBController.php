7bd5a4a0e3c89adbda7ab80762d20021657b81f1
quangvv-dev <quangphuong9685@gmail.com>
2022-03-11 16:41:29 +0700

    database v2

--- app/Http/Controllers/BE/DBController.php
+++ app/Http/Controllers/BE/DBController.php
@@ -13,10 +13,12 @@ class DBController extends Controller
 {
     public function index(Request $request)
     {
-        $status = Functions::getStatusWithCode('nguoi_mua_hang');
-        $order = Order::select('member_id', \DB::raw('COUNT(member_id) AS total'))->where('branch_id', $request->branch)
-            ->groupBy('member_id')->havingRaw('total = 1')->pluck('member_id');
-        Customer::whereIn('id', $order)->update(['status_id' => $status]);
+        for ($i = 1; $i < 17; $i++) {
+            $status = Functions::getStatusWithCode('nguoi_mua_hang');
+            $order = Order::select('member_id', \DB::raw('COUNT(member_id) AS total'))->where('branch_id', $i)
+                ->groupBy('member_id')->havingRaw('total = 1')->pluck('member_id');
+            Customer::whereIn('id', $order)->update(['status_id' => $status]);
+        }
         return "OKIE";
     }
 }
