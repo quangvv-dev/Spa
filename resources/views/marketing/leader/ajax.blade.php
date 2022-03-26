80870bf3417f4261a00879aed00a9f5e05ba83ed
quangvv-dev <quangphuong9685@gmail.com>
2022-03-26 15:34:20 +0700

    mkt dashbroad v2

--- resources/views/marketing/leader/ajax.blade.php
+++ resources/views/marketing/leader/ajax.blade.php
@@ -50,13 +50,13 @@
                 <tr class="">
                     <td class="text-center pdr10">{{$i}}</td>
                     <td class="text-center pdr10">{{$item->full_name}}
-                    <td class="text-center pdr10">0</td> {{--ngân sách--}}
+                    <td class="text-center pdr10">{{number_format($item->budget)}}</td> {{--ngân sách--}}
                     <td class="text-center pdr10">{{number_format($item->contact)}}</td>
-                    <td class="text-center pdr10">0</td>{{--giá contact--}}
+                    <td class="text-center pdr10">{{!empty($item->budget)&& !empty($item->contact)?$item->budget/$item->contact:0}}</td>{{--giá contact--}}
                     <td class="text-center pdr10">{{number_format($item->schedules)}}</td>
                     <td class="text-center pdr10">{{number_format($item->schedules_den)}}</td>
                     <td class="text-center pdr10">{{number_format($item->orders)}}</td>
-                    <td class="text-center pdr10">{{!empty($item->schedules_den) && !empty($item->gross_revenue) ?number_format($item->gross_revenue/$item->schedules_den):0 }}</td>
+                    <td class="text-center pdr10">{{!empty($item->schedules_den) && !empty($item->budget) ?number_format($item->budget/$item->schedules_den):0 }}</td>
                     <td class="text-center pdr10">{{!empty($item->schedules_den) && !empty($item->contact) ?round(($item->schedules_den/$item->contact)*100,1):0}}%</td>
 
                     <td class="text-center pdr10">{{number_format($item->all_total)}}</td>
