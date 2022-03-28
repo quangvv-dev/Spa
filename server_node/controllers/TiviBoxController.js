b8ec7d87473daf6d81619e14209537b8e6ac3cd6
quangvv-dev <quangphuong9685@gmail.com>
2022-03-28 17:41:30 +0700

    date current test

--- server_node/controllers/TiviBoxController.js
+++ server_node/controllers/TiviBoxController.js
@@ -10,8 +10,6 @@ function userExists(seeding_number, arr) {
 }
 
 
-
-
 /**
  * Nhập liệu khách hàng từ tin nhắn
  *
@@ -42,7 +40,7 @@ exports.SetCustomers = (phone, recipientId, text, senderId) => {
                 } finally {
                     //create Customer
                     console.log(name, 'Name Customer');
-                    const created_at = new Date(new Date().toLocaleString("en-GB", {timeZone: "Asia/Ho_Chi_Minh"}));
+                    const created_at = new Date(new Date().toString("en-US", {timeZone: "Asia/Ho_Chi_Minh"}));
                     console.log(' Time zone Asian',created_at);
                     model.CheckSource(rows[0].source_id, function (err, row2) {
                         if (err) {
