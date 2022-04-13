8485f024e4b028fd790b4bce0ff7e7fe54725b29
quangvv-dev <quangphuong9685@gmail.com>
2022-04-13 09:43:44 +0700

    fix demo

--- app/Http/Controllers/BE/DBController.php
+++ app/Http/Controllers/BE/DBController.php
@@ -7,18 +7,25 @@ use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\Order;
 use App\Helpers\Functions;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class DBController extends Controller
 {
     public function index(Request $request)
     {
-        for ($i = 1; $i < 17; $i++) {
-            $status = Functions::getStatusWithCode('khach_hang_vip');
-            $order = Order::select('member_id','gross_revenue', \DB::raw('SUM(gross_revenue) AS total'))->where('branch_id', $i)
-                ->groupBy('member_id')->havingRaw('total > 20000000')->pluck('member_id');
-            Customer::whereIn('id', $order)->update(['status_id' => $status]);
-        }
+//        for ($i = 1; $i < 17; $i++) {
+//            $status = Functions::getStatusWithCode('khach_hang_vip');
+//            $order = Order::select('member_id','gross_revenue', \DB::raw('SUM(gross_revenue) AS total'))->where('branch_id', $i)
+//                ->groupBy('member_id')->havingRaw('total > 20000000')->pluck('member_id');
+//            Customer::whereIn('id', $order)->update(['status_id' => $status]);
+//        }
+     $role =  Role::with('users')->get();
+     foreach ($role as $item){
+         if ($item->users()){
+             $item->users()->update(['department_id'=>$item->department_id]);
+         }
+     }
         return "OKIE";
     }
 }
