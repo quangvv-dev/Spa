b1edf475c987dd6179a2b1ec4e59c1f41638763d
Adamtech Admin <quangphuong9685@gmail.com>
2022-04-21 23:12:02 +0700

    format don hang

--- resources/views/order/payment-history.blade.php
+++ resources/views/order/payment-history.blade.php
@@ -17,7 +17,7 @@
                     <td class="fz-12">{{ date('d-m-Y', strtotime($item->payment_date)) }}
                     </td>
                     <td class="fz-12"><span style="color: grey;font-size: 10px">({{$item->payment_type==1?'Tiền mặt':
-                    ($item->payment_type==2?'Thẻ':($item->payment_type==3?'Điểm':'Chuyển khoản'))}})</span>  {{ number_format($item->price) }}</td>
+                    ($item->payment_type==2?'Thẻ':($item->payment_type==3?'Điểm':'C.Khoản'))}})</span>  {{ number_format($item->price) }}</td>
                     <td class="fz-12">{{ $item->description }}</td>
                     <td class="fz-12">
                         <a title="Xóa" class="btn delete" href="javascript:void(0)"
--- resources/views/payment_wallet/payment-history.blade.php
+++ resources/views/payment_wallet/payment-history.blade.php
@@ -15,7 +15,7 @@
             @foreach($payment as $item)
                 <tr>
                     <td class="fz-12">{{ date('d-m-Y', strtotime($item->payment_date)) }}</td>
-                    <td class="fz-12"><span style="color: grey;font-size: 10px">({{$item->payment_type==0?'Tiền mặt':'Chuyển khoản'}})</span>  {{ number_format($item->price) }}</td>
+                    <td class="fz-12"><span style="color: grey;font-size: 10px">({{$item->payment_type==0?'Tiền mặt':'C.Khoản'}})</span>  {{ number_format($item->price) }}</td>
                     <td class="fz-12">{{ $item->description }}</td>
                     <td class="fz-12">
                         <a title="Xóa" class="btn delete" href="javascript:void(0)"
