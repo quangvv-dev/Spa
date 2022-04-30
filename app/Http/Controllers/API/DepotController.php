b90ed031a3cc1658834b417b78e4f0e9ea5f6a6b
Adamtech Admin <quangphuong9685@gmail.com>
2022-04-30 10:39:05 +0700

    fix kho

--- app/Http/Controllers/API/DepotController.php
+++ app/Http/Controllers/API/DepotController.php
@@ -42,7 +42,9 @@ class DepotController extends BaseApiController
         ])->get()->pluck('id');
 
         $docs = ProductDepot::select('branch_id', 'product_id', 'quantity')
-            ->when(isset($input['branch_id']) && $input['branch_id'], function ($q) use ($input, $orders) {
+            ->when(isset($input['product_id']) && $input['product_id'], function ($q) use ($input, $orders) {
+                $q->where('product_id', $input['product_id']);
+            })->when(isset($input['branch_id']) && $input['branch_id'], function ($q) use ($input, $orders) {
                 $q->where('branch_id', $input['branch_id']);
             })->get()->map(function ($item) use ($input, $orders) {
                 $item->xuat_ban = OrderDetail::select('quantity')->whereIn('order_id', $orders)->where('booking_id', $item->product_id)
