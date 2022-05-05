1560e752385442dccd8408cfad9b0557f51146d3
quangvv-dev <quangphuong9685@gmail.com>
2022-05-05 11:20:59 +0700

    partition table

--- database/migrations/2022_05_05_105304_add_password_customer.php
+++ database/migrations/2022_05_05_105304_add_password_customer.php
@@ -14,7 +14,7 @@ class AddPasswordCustomer extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            //
+            $table->text('password')->comment('Mật khẩu')->nullable()->after('phone');
         });
     }
 
