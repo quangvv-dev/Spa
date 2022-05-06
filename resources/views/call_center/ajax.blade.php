626676473e67cad52989f056fe3f15c3a0ace802
quangvv-dev <quangphuong9685@gmail.com>
2022-05-06 11:27:07 +0700

    tai tong dai

--- resources/views/call_center/ajax.blade.php
+++ resources/views/call_center/ajax.blade.php
@@ -54,8 +54,8 @@
                             <div class="mediPlayer">
                                 <audio class="listen" preload="none" data-size="40"
                                        src="{{$doc->recording_url}}"></audio>
+                                <a href="{{$doc->recording_url}}"><i class="fa fa-download"></i></a>
                             </div>
-                            <a href="{{$doc->recording_url}}"><i class="fa fa-2x fa-download"></i></a>
                         @endif
                     </td>
                 </tr>
