3ff162ac6e9629784b2e9ac0c6d903a911608270
Tien Nguyen <tiennm298@gmail.com>
2022-05-25 18:27:20 +0700

    add filter phone

---
+++ database/migrations/2022_03_22_104503_danh_index_all_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DanhIndexAllTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->integer('mkt_id')->index()->change();
+            $table->integer('source_id')->index()->change();
+            $table->bigInteger('source_fb')->index()->change();
+            $table->bigInteger('membership')->index()->change();
+            $table->bigInteger('page_id')->index()->change();
+        });
+        Schema::table('schedules', function (Blueprint $table) {
+            $table->integer('user_id')->index()->change();
+            $table->integer('status')->index()->change();
+            $table->integer('creator_id')->index()->change();
+            $table->integer('person_action')->index()->change();
+            $table->integer('category_id')->index()->change();
+        });
+        Schema::table('payment_histories', function (Blueprint $table) {
+            $table->bigInteger('order_id')->index()->change();
+            $table->integer('payment_type')->index()->change();
+        });
+//        Schema::table('customers', function (Blueprint $table) {
+//            $table->integer('page_id')->index()->change();
+//
+//        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
