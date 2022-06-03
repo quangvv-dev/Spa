d489e4e58c435ee191661063998a72f18e641c97
quangvv-dev <quangphuong9685@gmail.com>
2022-06-03 10:29:45 +0700

    change flow MKT

--- app/Http/Controllers/BE/CustomerController.php
+++ app/Http/Controllers/BE/CustomerController.php
@@ -71,7 +71,7 @@ class CustomerController extends Controller
         $group = Category::pluck('name', 'id')->toArray();//nhóm KH
         $source = Status::where('type', StatusCode::SOURCE_CUSTOMER)->select('name', 'id')->pluck('name', 'id')->toArray();// nguồn KH
         $branchs = Branch::search()->pluck('name', 'id');// chi nhánh
-        $marketingUsers = User::whereIn('department_id', [DepartmentConstant::MARKETING, DepartmentConstant::CARE_PAGE])->select('full_name', 'id')->pluck('full_name', 'id')->toArray();
+        $marketingUsers = User::whereIn('department_id', [DepartmentConstant::CARE_PAGE])->select('full_name', 'id')->pluck('full_name', 'id')->toArray();
         $genitives = Genitive::select('id', 'name')->pluck('name', 'id')->toArray();
         $telesales = [];
         User::select('id','department_id','full_name')->get()->map(function ($item) use (&$telesales) {
