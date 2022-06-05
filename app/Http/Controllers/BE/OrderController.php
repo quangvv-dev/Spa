ee9731e38bc8e799ab51ad6a3f513cbddaca15ea
quangphuong9685 <quangphuong9685@gmail.com>
2022-06-05 11:25:45 +0700

    fix loi lang ha

--- app/Http/Controllers/BE/OrderController.php
+++ app/Http/Controllers/BE/OrderController.php
@@ -617,9 +617,8 @@ class OrderController extends Controller
         ]);
 
         if ($request->type_delete == StatusCode::TYPE_ORDER_PROCESS) {
-            $order->update([
-                'count_day' => $order->count_day - 1,
-            ]);
+            $order->count_day = $order->count_day - 1;
+            $order->save();
 
             $order_detail = OrderDetail::where('order_id', $order->id)->where('booking_id',
                 $request->service_id)->first();
@@ -647,9 +646,8 @@ class OrderController extends Controller
         }
 
         if ($request->type == StatusCode::TYPE_ORDER_PROCESS) {
-            $order->update([
-                'count_day' => $order->count_day + 1,
-            ]);
+            $order->count_day = $order->count_day + 1;
+            $order->save();
         }
 
         HistoryUpdateOrder::where('id', $request->history_id)->delete();
