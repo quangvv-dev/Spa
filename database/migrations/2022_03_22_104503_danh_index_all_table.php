1c04665fe932721d3b6fd48d96bbfbf3e478ed39
quangphuong9685 <quangphuong9685@gmail.com>
2022-06-17 00:03:48 +0700

    danh index

--- database/migrations/2022_03_22_104503_danh_index_all_table.php
+++ database/migrations/2022_03_22_104503_danh_index_all_table.php
@@ -13,24 +13,24 @@ class DanhIndexAllTable extends Migration
      */
     public function up()
     {
-        Schema::table('customers', function (Blueprint $table) {
-            $table->integer('mkt_id')->index()->change();
+//        Schema::table('customers', function (Blueprint $table) {
+//            $table->integer('mkt_id')->index()->change();
 //            $table->integer('source_id')->index()->change();
-            $table->bigInteger('source_fb')->index()->change();
-            $table->bigInteger('membership')->index()->change();
-            $table->bigInteger('page_id')->index()->change();
-        });
-        Schema::table('schedules', function (Blueprint $table) {
-            $table->integer('user_id')->index()->change();
-            $table->integer('status')->index()->change();
-            $table->integer('creator_id')->index()->change();
-            $table->integer('person_action')->index()->change();
-            $table->integer('category_id')->index()->change();
-        });
-        Schema::table('payment_histories', function (Blueprint $table) {
-            $table->bigInteger('order_id')->index()->change();
-            $table->integer('payment_type')->index()->change();
-        });
+//            $table->bigInteger('source_fb')->index()->change();
+//            $table->bigInteger('membership')->index()->change();
+//            $table->bigInteger('page_id')->index()->change();
+//        });
+//        Schema::table('schedules', function (Blueprint $table) {
+//            $table->integer('user_id')->index()->change();
+//            $table->integer('status')->index()->change();
+//            $table->integer('creator_id')->index()->change();
+//            $table->integer('person_action')->index()->change();
+//            $table->integer('category_id')->index()->change();
+//        });
+//        Schema::table('payment_histories', function (Blueprint $table) {
+//            $table->bigInteger('order_id')->index()->change();
+//            $table->integer('payment_type')->index()->change();
+//        });
 //        Schema::table('customers', function (Blueprint $table) {
 //            $table->integer('page_id')->index()->change();
 //
