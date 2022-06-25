7bde13a3c4ed2d5c52644a8807c921dab1efdfd5
quangvv-dev <quangphuong9685@gmail.com>
2022-06-25 16:26:44 +0700

    thong bao notification

--- app/Models/NotificationCustomer.php
+++ app/Models/NotificationCustomer.php
@@ -9,14 +9,14 @@ class NotificationCustomer extends Model
     protected $guarded = ['id'];
     protected $table = 'notifications_customers';
     
-//    public function getDataAttribute($data)
-//    {
-//        return @json_decode((array)$data, true);
-//    }
-//
-//    public function setDataAttribute($data)
-//    {
-//        return @json_encode((array)$data, true);
-//    }
+    public function getDataAttribute()
+    {
+        return @json_decode((array)$this->data, true);
+    }
+
+    public function setDataAttribute()
+    {
+        return @json_encode((array)$this->data, true);
+    }
 
 }
