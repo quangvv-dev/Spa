27f9c3253d8658c4f038e1a017ef457a0420097b
quangvv-dev <quangphuong9685@gmail.com>
2022-06-25 16:59:15 +0700

    yeu cau rut tien

--- database/migrations/2022_06_25_160418_create_receive_money_table.php
+++ database/migrations/2022_06_25_160418_create_receive_money_table.php
@@ -15,7 +15,10 @@ class CreateReceiveMoneyTable extends Migration
     {
         Schema::create('receive_money', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('customer_id')->comment("người yêu cầu rút tiền");
+            $table->integer('customer_id')->index()->comment("người yêu cầu rút tiền");
+            $table->bigInteger('price')->comment("Số tiền muốn rút");
+            $table->longText('description')->comment("STK ngân hàng");
+            $table->integer('status')->index()->comment("0: chờ duyệt; 1: duyệt; 2: Từ chối");
             $table->timestamps();
         });
     }
