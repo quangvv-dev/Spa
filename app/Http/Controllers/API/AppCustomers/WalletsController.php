f33ce14dc186ca439cea94d1c4bbdc9f8b4e340d
quangphuong9685 <quangphuong9685@gmail.com>
2022-06-27 00:17:49 +0700

    Y/c rut tien v3

--- app/Http/Controllers/API/AppCustomers/WalletsController.php
+++ app/Http/Controllers/API/AppCustomers/WalletsController.php
@@ -33,7 +33,7 @@ class WalletsController extends BaseApiController
     {
         $customer = $request->jwtUser;
         $records = isset($request->records) ? $request->records : StatusCode::PAGINATE_10;
-        $history = HistoryWalletCtv::select('id', 'customer_id', 'price', 'type', 'created_at')
+        $history = HistoryWalletCtv::select('id', 'customer_id', 'price', 'type','status','description','created_at')
             ->where('customer_id', $customer->id)->when(isset($request->status) && $request->status,
                 function ($qr) use ($request) {
                     $qr->where('status', $request->status);
