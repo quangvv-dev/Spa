d3688742a44611bfe5df8ce21c23a3094c2c3251
quangvv-dev <quangphuong9685@gmail.com>
2022-07-16 17:17:55 +0700

    thu no

--- app/Http/Controllers/BE/Marketing/CarepageController.php
+++ app/Http/Controllers/BE/Marketing/CarepageController.php
@@ -67,7 +67,7 @@ class CarepageController extends Controller
                 $item->schedules = 0;
                 $item->schedules_den = 0;
             }
-            $orders = Order::searchAll($input)->select('id', 'gross_revenue', 'all_total');
+            $orders = Order::searchAll($input)->select('id', 'order_id', 'gross_revenue', 'all_total');
             $payment = PaymentHistory::search($input, 'price')->whereHas('order', function ($qr) use ($input) {
                 $qr->where('carepage_id', $input['carepage_id']);
             });
