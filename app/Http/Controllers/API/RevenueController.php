3edfaa2c13858a61f03b82680ade3604174dadc8
quangvv-dev <quangphuong9685@gmail.com>
2022-07-21 17:11:48 +0700

    fix api thu chi toan he thong

--- app/Http/Controllers/API/RevenueController.php
+++ app/Http/Controllers/API/RevenueController.php
@@ -518,17 +518,30 @@ class RevenueController extends BaseApiController
         }
         $request->merge(['type_api' => 'all_branch']);
         $input = $request->except('type_api');
-        $data = Order::returnRawData($input)->select('branch_id', \DB::raw('SUM(all_total) AS total'),
-            \DB::raw('SUM(gross_revenue) AS revenue'))
-            ->groupBy('branch_id')->where('branch_id', '<>', 0)->get()->map(function ($item) use ($input) {
-                $params = $input;
-                $params['branch_id'] = $item->branch_id;
-                $payment = PaymentHistory::search($params, 'price');
-                $item->payment = $payment->sum('price');
-                $item->name = @$item->branch->name;
-                unset($item->branch);
-                return $item;
-            })->sortByDesc('payment');
+        $data = Branch::select('id','name')->get()->map(function ($item) use ($input){
+            $input['branch_id'] = $item->id;
+            $payment = PaymentHistory::search($input, 'price');
+            $payment_wallet = PaymentWallet::search($input, 'price');
+            $thu = $payment->sum('price') + $payment_wallet->sum('price');
+            $chi = ThuChi::search($input)->where('status', UserConstant::ACTIVE)->sum('so_tien');
+            $item->total = (int) $thu;
+            $item->revenue = (int) $chi;
+            $item->payment = (int)$thu - (int)$chi;
+            return $item;
+        })->sortByDesc('payment');
+
+//        $data = Order::returnRawData($input)->select('branch_id', \DB::raw('SUM(all_total) AS total'),
+//            \DB::raw('SUM(gross_revenue) AS revenue'))
+//            ->groupBy('branch_id')->where('branch_id', '<>', 0)->get()->map(function ($item) use ($input) {
+//                $params = $input;
+//                $params['branch_id'] = $item->branch_id;
+//                $payment = PaymentHistory::search($params, 'price');
+//                $item->payment = $payment->sum('price');
+//                $item->name = @$item->branch->name;
+//                unset($item->branch);
+//                return $item;
+//            })->sortByDesc('payment');
+
         $data = ChartResource::collection($data);
 
         return $this->responseApi(ResponseStatusCode::OK, 'SUCCESS', $data);
