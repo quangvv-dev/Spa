b4b84d8f30c883c1adaa0cf020ad86b536504e48
quangvv-dev <quangphuong9685@gmail.com>
2022-11-07 17:35:02 +0700

    an sdt

--- resources/views/customers/ajax.blade.php
+++ resources/views/customers/ajax.blade.php
@@ -228,7 +228,7 @@
                         <span class="noti-number noti-number-on ml5">{{ $customer->groupComments->count() }}</span>
                     </td>
                     <td class="text-center phone-customer" data-customer-id="{{ $customer->id }}">
-                        <a href="callto:{{ $customer->phone }}">{{ $customer->phone }}</a>
+                        <a href="callto:{{ $customer->phone }}">{{ str_limit($customer->phone,7,'xxx') }}</a>
 
                         @if(!empty($customer->call_back))
                             <span><i class="fas fa-phone call-back" data-id="{{$customer->call_back}}" style="cursor: pointer;color: red !important;"></i></span>
