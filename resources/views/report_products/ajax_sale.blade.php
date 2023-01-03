6612e5638813c91fbf8ac79ca9714f7b1b6fe8ac
quangvv-dev <quangphuong9685@gmail.com>
2023-01-03 16:10:34 +0700

    telesale moi

--- resources/views/report_products/ajax_sale.blade.php
+++ resources/views/report_products/ajax_sale.blade.php
@@ -1,42 +1,22 @@
 <div style="width: 100%; overflow: auto;margin-top: 20px;height: 900px;" class="tableFixHead">
     <table class="table table-bordered table-info hidden-xs" style="margin-bottom: 0px;">
         <thead class="bg-primary text-white">
-        <tr>
-            <th class="text-center" colspan="1">STT</th>
-            <th class="text-center" colspan="1">SALE</th>
-            <th class="text-center" colspan="14">KHÁCH HÀNG MỚI</th>
-            {{--<th class="text-center" colspan="6">KHÁCH HÀNG CŨ</th>--}}
-            {{--<th class="text-center" colspan="5">TỔNG CHUNG</th>--}}
-        </tr>
         <tr class="tr1" style="text-transform:unset">
             <th class="text-center"></th>
             <th class="text-center">Nhân viên</th>
-            <th class="text-center no-wrap">SĐT</th>
-            <th class="text-center">Trao đổi</th>
+            <th class="text-center no-wrap">Tác nghiệp</th>
+            <th class="text-center">Số mới</th>
+            <th class="text-center">Tiếp cận</th>
             <th class="text-center">Lịch hẹn</th>
-            <th class="text-center">Khách đến</th>
-            <th class="text-center">Đơn chốt</th>
-            <th class="text-center">Tỷ lệ<span class=""><br>chốt lịch</span></th>
-            <th class="text-center">Tỷ lệ đến</th>
-            <th class="text-center">Tỷ lệ<span class=""><br>chốt đơn</span></th>
-            <th class="text-center">Tỷ lệ<span class=""><br>chốt đơn /SĐT</span></th>
-            <th class="text-center">TB đơn</th>
-            <th class="text-center">Doanh số<span class=""><br>sau CK</span></th>
-            <th class="text-center">Doanh thu</th>
-            <th class="text-center no-wrap">Thu nợ</th>
-            <th class="text-center no-wrap">Thực thu</th>
-            {{--<th class="text-center no-wrap">Trao đổi</th>--}}
-            {{--<th class="text-center no-wrap">Đơn chốt</th>--}}
-            {{--<th class="text-center">Doanh số<span class=""><br>sau CK</span></th>--}}
-            {{--<th class="text-center">Doanh thu</th>--}}
-            {{--<th class="text-center">Thu nợ</th>--}}
-            {{--<th class="text-center">Thực thu</th>--}}
-            {{--<th class="text-center">Doanh thu<span class=""><br>TB/đơn</span></th>--}}
-            {{--<th class="text-center">Doanh<span class=""><br>số</span></th>--}}
-            {{--<th class="text-center">Doanh<span class=""><br>thu</span></th>--}}
-            {{--<th class="text-center">Thu nợ</th>--}}
-            {{--<th class="text-center">Đã thu<span class=""><br>T.kỳ</span></th>--}}
-        </tr>
+            <th class="text-center">Lịch hủy</th>
+            <th class="text-center">HV đến</th>
+            <th class="text-center">HV chốt</th>
+            <th class="text-center">Tỷ lệ<span class=""><br>tiếp cận(%)</span></th>
+            <th class="text-center">% Lịch hẹn/tiếp cận</th>
+            <th class="text-center">% Hủy lịch</th>
+            <th class="text-center">% HV đến/Lịch hẹn</th>
+            <th class="text-center">% HV chốt/HV đến</th>
+            <th class="text-center">% Chốt/SĐT</th>
         <tr class="number_index">
             <th class="text-center">(1)</th>
             <th class="text-center">(2)</th>
@@ -44,123 +24,58 @@
             <th class="text-center">(4)</th>
             <th class="text-center">(5)</th>
             <th class="text-center">(6)</th>
-            <th class="text-center">(6.1)</th>
-            <th class="text-center">(5)/(3)</th>
-            <th class="text-center">(6)/(5)</th>
-            <th class="text-center">(6.1)/(6)</th>
-            <th class="text-center">(6.1)/(3)</th>
-            <th class="text-center">(10)/(6.1)</th>
+            <th class="text-center">()</th>
+            <th class="text-center">()</th>
+            <th class="text-center">()</th>
+            <th class="text-center">()</th>
+            <th class="text-center">()</th>
+            <th class="text-center">()</th>
             <th class="text-center">(7)</th>
             <th class="text-center">(8)</th>
             <th class="text-center">(9)</th>
-            <th class="text-center">(10)</th>
-            {{--<th class="text-center">(11)</th>--}}
-            {{--<th class="text-center">(12)</th>--}}
-            {{--<th class="text-center">(13)</th>--}}
-            {{--<th class="text-center">(14)</th>--}}
-            {{--<th class="text-center">(15)</th>--}}
-            {{--<th class="text-center">(14)+(15)</th>--}}
-            {{--<th class="text-center">(7)+(12)</th>--}}
-            {{--<th class="text-center">(8)+(13)</th>--}}
-            {{--<th class="text-center"></th>--}}
-            {{--<th class="text-center"></th>--}}
-            {{--<th class="text-center"></th>--}}
         </tr>
         </thead>
 
         <tbody>
-        @php
-            $i=0;
-            $customer_new = 0;
-            $comment_new = 0;
-            $order_new = 0;
-            $schedules_percent = 0;
-            $revenue_new = 0;
-            $payment_new = 0;
-            $all_detail_new = 0;
-            $schedules_new = 0;
-            $all_schedules_den = 0;
-        @endphp
 
         @if(count($users))
             @foreach($users as $item)
-                @php $i++ ;
-                $customer_new += $item->customer_new;
-                $comment_new += $item->comment_new;
-                $order_new += $item->order_new;
-                $schedules_percent += $item->schedules_den >0 && $item->customer_new>0 ?round(($item->schedules_new/$item->customer_new)*100,1):0;
-                $revenue_new += $item->revenue_new;
-                $payment_new += $item->payment_new;
-                $schedules_new += $item->schedules_new;
-                $all_schedules_den += $item->schedules_den;
-                $all_detail_new += $item->detail_new;
-
-                @endphp
                 <tr class="">
-                    <td class="text-center pdr10">{{$i}}</td>
+                    <td class="text-center pdr10"></td>
                     <td class="text-center pdr10">{{$item->full_name}}
+                    <td class="text-center pdr10">{{$item->call_center}}</td>
                     <td class="text-center pdr10">{{$item->customer_new}}</td>
-                    <td class="text-center pdr10">{{$item->comment_new}}</td>
-                    <td class="text-center pdr10">{{$item->schedules_new}}</td>
-                    <td class="text-center pdr10">{{$item->schedules_den}}</td>
-                    <td class="text-center pdr10">{{$item->order_new}}</td>
-                    <td class="text-center pdr10">{{!empty($item->schedules_new) && !empty($item->customer_new) ?round(($item->schedules_new/$item->customer_new)*100,1):0}}%</td>
-                    <td class="text-center pdr10">{{!empty($item->schedules_den) && !empty($item->schedules_new) ? round($item->schedules_den/$item->schedules_new*100,1):0}}%</td>
-                    <td class="text-center pdr10">{{$item->order_new>0&&$item->schedules_den >0 ?round(($item->order_new/$item->schedules_den)*100,1):0}}%</td>
-                    <td class="text-center pdr10">{{$item->order_new>0&&$item->customer_new >0 ?round(($item->order_new/$item->customer_new)*100,1):0}}%</td>
-                    <td class="text-center pdr10">{{$item->order_new>0&&$item->detail_new >0 ?number_format($item->detail_new/$item->order_new):0}}</td>
-                    <td class="text-center pdr10">{{number_format($item->revenue_new)}}</td>
-                    <td class="text-center pdr10">{{number_format($item->payment_new)}}</td>
-                    <td class="text-center pdr10">{{number_format($item->detail_new - $item->payment_new)}}</td>
-                    <td class="text-center pdr10">{{number_format($item->detail_new)}}</td>
-
-
-                    {{--<td class="text-center pdr10">{{$item->comment_old}}</td>--}}
-                    {{--<td class="text-center pdr10">{{$item->order_old}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format($item->revenue_old)}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format($item->payment_old)}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format($item->all_payment - $item->detail_new -$item->payment_old)}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format($item->all_payment - $item->detail_new)}}</td>--}}
-                    {{--<td class="text-center pdr10">{{!empty($item->payment_old) && !empty($item->order_old) ? number_format($item->payment_old/$item->order_old):0}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format($item->revenue_total)}}</td>--}}
-
-                    {{--<td class="text-center pdr10">{{number_format($item->payment_new + $item->payment_old)}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format(($item->all_payment - $item->payment_new - $item->payment_old)>0?$item->all_payment - $item->payment_new - $item->payment_old:0)}}</td>--}}
-                    {{--<td class="text-center pdr10">{{number_format($item->all_payment)}}</td>--}}
+                    <td class="text-center pdr10">{{number_format($item->tiep_can)}}</td>
+                    <td class="text-center pdr10">{{$item->all_schedules}}</td>
+                    <td class="text-center pdr10">{{$item->schedules_huy}}</td>
+                    <td class="text-center pdr10">{{number_format($item->schedules_den)}}</td>
+                    <td class="text-center pdr10">{{number_format($item->orders)}}</td>
+                    <td class="text-center pdr10">{{!empty($item->tiep_can) && !empty($item->customer_new)?($item->tiep_can/$item->customer_new)*100:0}}%</td>
+                    <td class="text-center pdr10">{{!empty($item->tiep_can) && !empty($item->all_schedules)?($item->all_schedules/$item->tiep_can)*100:0}}%</td>
+                    <td class="text-center pdr10">{{number_format($item->schedules_huy)}}</td>
+                    <td class="text-center pdr10">{{!empty($item->schedules_den)?($item->schedules_den/$item->all_schedules)*100:0}}%</td>
+                    <td class="text-center pdr10">{{!empty($item->orders) && !empty($item->schedules_den)?($item->orders/$item->schedules_den)*100:0}}%</td>
+                    <td class="text-center pdr10">{{!empty($item->orders) && !empty($item->customer_new)?($item->orders/$item->customer_new)*100:0}}%</td>
                 </tr>
             @endforeach
         @endif
-        <tr>
-            <td class="text-center"></td>
-            <td class="text-center bold">Tổng cộng</td>
-            <td class="text-center bold">{{@number_format($customer_new)}}</td>
-            <td class="text-center bold">{{@number_format($comment_new)}}</td>
-            <td class="text-center bold">{{@number_format($schedules_new)}}</td>
-            <td class="text-center bold">{{@number_format($all_schedules_den)}}</td>
-            <td class="text-center bold">{{@number_format($order_new)}}</td>
-            <td class="text-center bold">{{!empty($schedules_new)&& !empty($customer_new)?round($schedules_new/$customer_new*100,1):0}}%</td>
-            <td class="text-center bold">{{!empty($all_schedules_den)&& !empty($schedules_new)?round($all_schedules_den/$schedules_new*100,1):0}}%</td>
-            <td class="text-center bold">{{!empty($all_schedules_den)&& !empty($order_new)?round($order_new/$all_schedules_den*100,1):0}}%</td>
-            <td class="text-center bold">{{!empty($customer_new)&& !empty($order_new)?round($order_new/$customer_new*100,1):0}}%</td>
-            <td class="text-center bold">{{!empty($all_detail_new)&& !empty($order_new)?number_format($all_detail_new/$order_new):0}}</td>
-            <td class="text-center bold">{{@number_format($revenue_new)}}</td>
-            <td class="text-center bold">{{@number_format($payment_new)}}</td>
-            <td class="text-center bold">{{@number_format($all_detail_new - $payment_new)}}</td>
-            <td class="text-center bold">{{@number_format($all_detail_new)}}</td>
-
-
-            {{--<td class="text-center bold">{{@number_format($comment_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($order_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($revenue_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($payment_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($all_payment - $all_detail_new - $payment_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($all_payment - $all_detail_new)}}</td>--}}
-            {{--<td class="text-center bold">{{!empty($payment_old)&& !empty($order_old)?number_format($payment_old/$order_old):0}}</td>--}}
-            {{--<td class="bold">{{number_format($allTotal)}}</td>--}}
-            {{--<td class="bold">{{number_format($payment_new + $payment_old)}}</td>--}}
-            {{--<td class="bold">{{number_format(($all_payment - $payment_new - $payment_old)>0?$all_payment - $payment_new - $payment_old:0)}}</td>--}}
-            {{--<td class="bold">{{number_format($all_payment)}}</td>--}}
-        </tr>
+        {{--<tr>--}}
+            {{--<td class="text-center"></td>--}}
+            {{--<td class="text-center bold">Tổng cộng</td>--}}
+            {{--<td class="text-center bold">{{@number_format($customer_new)}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($comment_new)}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($schedules_new)}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($all_schedules_den)}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($order_new)}}</td>--}}
+            {{--<td class="text-center bold">{{!empty($schedules_new)&& !empty($customer_new)?round($schedules_new/$customer_new*100,1):0}}%</td>--}}
+            {{--<td class="text-center bold">{{!empty($all_schedules_den)&& !empty($schedules_new)?round($all_schedules_den/$schedules_new*100,1):0}}%</td>--}}
+            {{--<td class="text-center bold">{{!empty($all_schedules_den)&& !empty($order_new)?round($order_new/$all_schedules_den*100,1):0}}%</td>--}}
+            {{--<td class="text-center bold">{{!empty($customer_new)&& !empty($order_new)?round($order_new/$customer_new*100,1):0}}%</td>--}}
+            {{--<td class="text-center bold">{{!empty($all_detail_new)&& !empty($order_new)?number_format($all_detail_new/$order_new):0}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($revenue_new)}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($payment_new)}}</td>--}}
+            {{--<td class="text-center bold">{{@number_format($all_detail_new - $payment_new)}}</td>--}}
+        {{--</tr>--}}
 
         </tbody>
     </table>
