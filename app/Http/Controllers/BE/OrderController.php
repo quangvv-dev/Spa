f2d28877a2a317099ea0d8fb2e43505ef1ea7964
Tien Nguyen <tiennm298@gmail.com>
2023-03-04 18:09:04 +0700

    order new

--- app/Http/Controllers/BE/OrderController.php
+++ app/Http/Controllers/BE/OrderController.php
@@ -70,12 +70,12 @@ class OrderController extends Controller
             Order::TYPE_ORDER_ADVANCE => 'Liệu trình',
         ];
 
-//        $spaTherapissts = User::select('id', 'avatar', 'full_name','percent_rose')->where('department_id', DepartmentConstant::DOCTOR)->get();
-//        $customer_support = User::select('id', 'avatar', 'full_name')->whereIn('department_id', [DepartmentConstant::TECHNICIANS, UserConstant::WAITER,DepartmentConstant::DOCTOR])->get();
+        $spaTherapissts = User::select('id', 'avatar', 'full_name','percent_rose')->where('department_id', DepartmentConstant::DOCTOR)->get();
+        $customer_support = User::select('id', 'avatar', 'full_name')->whereIn('department_id', [DepartmentConstant::TECHNICIANS, UserConstant::WAITER,DepartmentConstant::DOCTOR])->get();
 
-        $spaTherapissts = User::select('id', 'avatar', 'full_name', 'percent_rose')->get();
-        $customer_support = User::select('id', 'avatar', 'full_name')->get();
-        $customer_y_ta = User::select('id', 'avatar', 'full_name')->get();
+//        $spaTherapissts = User::select('id', 'avatar', 'full_name', 'percent_rose')->get();
+//        $customer_support = User::select('id', 'avatar', 'full_name')->get();
+//        $customer_y_ta = User::select('id', 'avatar', 'full_name')->get();
 
 
         $branchs = Branch::search()->pluck('name', 'id');
