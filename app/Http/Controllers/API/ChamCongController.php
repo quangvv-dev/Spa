6b7991a863c2d69d49f69e447f58bf3046ffa9a1
quangphuong9685 <quangphuong9685@gmail.com>
2023-04-20 19:10:52 +0700

    fix cham cong

--- app/Http/Controllers/API/ChamCongController.php
+++ app/Http/Controllers/API/ChamCongController.php
@@ -15,32 +15,30 @@ class ChamCongController extends BaseApiController
     {
 //        $data = "{\"NameMachine\":\"HN1\",\"Info\":[{\"MachineNumber\":\"1\",\"DateTimeRecord\":\"2\/6\/2023 1:58:07 PM\",\"IndRedID\":\"1\"},{\"MachineNumber\":\"1\",\"DateTimeRecord\":\"2\/6\/2023 2:10:11 PM\",\"IndRedID\":\"1\"},{\"MachineNumber\":\"1\",\"DateTimeRecord\":\"2\/6\/2023 2:10:19 PM\",\"IndRedID\":\"1\"},{\"MachineNumber\":\"1\",\"DateTimeRecord\":\"2\/7\/2023 8:54:42 AM\",\"IndRedID\":\"1\"}]}";
         $data = $request->all();
-        \DB::table('settings')->insert(['setting_key'=>'cham_cong','setting_value'=>\GuzzleHttp\json_encode($data)]);
-
         $input = [];
-//        $array_approval_code = User::pluck('approval_code')->toArray();
+        $array_approval_code = User::select('approval_code')->whereNotNull('approval_code')->pluck('approval_code')->toArray();
 
         foreach ($data['Info'] as $item) {
             $date = Carbon::parse($item['DateTimeRecord'])->format('Y-m-d H:i:s');
-            $isset = ChamCong::where('name_machine', $data['NameMachine'])->where('ind_red_id', $item['IndRedID'])->where('date_time_record', $date)->first();
-            $approval_code = $data['NameMachine'].'.'.$item['IndRedID'];
+            $isset = ChamCong::where('name_machine', $data['NameMachine'])->where('ind_red_id',
+                $item['IndRedID'])->where('date_time_record', $date)->first();
+            $approval_code = $data['NameMachine'] . '.' . $item['IndRedID'];
 
-//            if(in_array($approval_code,$array_approval_code)){
-            if (empty($isset)) {
-                $input[] = [
-                    'name_machine'     => $data['NameMachine'],
-                    'machine_number'   => $item['MachineNumber'],
-                    'date_time_record' => $date,
-                    'ind_red_id'       => $item['IndRedID'],
-                    'approval_code'    => $approval_code,
-                    'created_at'       => Carbon::now('Asia/Ho_Chi_Minh')->format('Y-m-d H:i'),
-                    'updated_at'       => Carbon::now('Asia/Ho_Chi_Minh')->format('Y-m-d H:i'),
-                ];
+            if (in_array($approval_code, $array_approval_code)) {
+                if (empty($isset)) {
+                    $input[] = [
+                        'name_machine'     => $data['NameMachine'],
+                        'machine_number'   => $item['MachineNumber'],
+                        'date_time_record' => $date,
+                        'ind_red_id'       => $item['IndRedID'],
+                        'approval_code'    => $approval_code,
+                        'created_at'       => Carbon::now('Asia/Ho_Chi_Minh')->format('Y-m-d H:i'),
+                        'updated_at'       => Carbon::now('Asia/Ho_Chi_Minh')->format('Y-m-d H:i'),
+                    ];
+                }
             }
-//            }
-
         }
         ChamCong::insert($input);
-        return $this->responseApi(ResponseStatusCode::OK,'Đẩy chấm công thành công!!');
+        return $this->responseApi(ResponseStatusCode::OK, 'Đẩy chấm công thành công!!');
     }
 }
