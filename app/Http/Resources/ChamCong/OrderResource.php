b71c225f8fc8208819186e508c2d3905e93cb915
quangphuong9685 <quangphuong9685@gmail.com>
2023-05-21 10:25:31 +0700

    DOn tu

--- app/Http/Resources/ChamCong/OrderResource.php
+++ app/Http/Resources/ChamCong/OrderResource.php
@@ -16,22 +16,22 @@ class OrderResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'          => $this->id,
-            'full_name'   => @$this->user->full_name,
-            'code'        => @$this->user->code,
-            'department'  => @$this->user->department->name,
-            'role'        => @$this->user->roles->name,
-            'status_text' => @$this->status == 1 ? "Đã duyệt" : ($this->status == 0 ? "Chờ duyệt" : 'Không duyệt'),
-            'status'      => $this->status,
-            'title'       => $this->type == 0 ? "Đơn nghỉ" : 'Đơn check-in/check-out',
-            'description' => @$this->description,
-            'date'        => @$this->date,
-            'date_end'    => @$this->date_end,
-            'reason_id'   => @$this->reason_id,
-            'reason_text' => @$this->reason->name,
-            'accept_id'   => @$this->accept_id,
-            'time_to'     => !empty($this->time_to) ? array_search($this->time_to, ChamCongConstant::HOURS) : '',
-            'time_end'    => !empty($this->time_end) ? array_search($this->time_end, ChamCongConstant::HOURS) : '',
+            'id'              => $this->id,
+            'full_name'       => @$this->user->full_name,
+            'code_user'       => @$this->user->code,
+            'department_name' => @$this->user->department->name,
+            'role_name'       => @$this->user->roles->name,
+            'status_text'     => @$this->status == 1 ? "Đã duyệt" : ($this->status == 0 ? "Chờ duyệt" : 'Không duyệt'),
+            'status'          => $this->status,
+            'title'           => $this->type == 0 ? "Đơn nghỉ" : 'Đơn check-in/check-out',
+            'description'     => @$this->description,
+            'date'            => @$this->date,
+            'date_end'        => @$this->date_end,
+            'reason_id'       => @$this->reason_id,
+            'reason_text'     => @$this->reason->name,
+            'accept_id'       => @$this->accept_id,
+            'time_to'         => !empty($this->time_to) ? array_search($this->time_to, ChamCongConstant::HOURS) : '',
+            'time_end'        => !empty($this->time_end) ? array_search($this->time_end, ChamCongConstant::HOURS) : '',
         ];
     }
 }
