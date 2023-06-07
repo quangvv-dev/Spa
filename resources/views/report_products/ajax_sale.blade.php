d39598849ebd18e5e565051131849b446608055a
quangvv-dev <quangphuong9685@gmail.com>
2023-06-07 17:14:24 +0700

    them chi so telesale

--- resources/views/report_products/ajax_sale.blade.php
+++ resources/views/report_products/ajax_sale.blade.php
@@ -1,13 +1,13 @@
 <div style="width: 100%; overflow: auto;margin-top: 20px;height: 900px;" class="tableFixHead">
     <table class="table table-bordered table-info hidden-xs" style="margin-bottom: 0px;">
         <thead class="bg-primary text-white">
-        <tr>
-            <th class="text-center" colspan="1">STT</th>
-            <th class="text-center" colspan="1">SALE</th>
-            <th class="text-center" colspan="1"></th>
-            <th class="text-center" colspan="16">KHÁCH HÀNG MỚI</th>
+{{--        <tr>--}}
+{{--            <th class="text-center" colspan="1">STT</th>--}}
+{{--            <th class="text-center" colspan="1">SALE</th>--}}
+{{--            <th class="text-center" colspan="1"></th>--}}
+{{--            <th class="text-center" colspan="18">KHÁCH HÀNG MỚI</th>--}}
 {{--            <th class="text-center" colspan="3">KHÁCH HÀNG CŨ</th>--}}
-        </tr>
+{{--        </tr>--}}
         <tr class="tr1" style="text-transform:unset">
             <th class="text-center"></th>
             <th class="text-center">Nhân viên</th>
@@ -16,11 +16,13 @@
             <th class="text-center">Lịch hẹn</th>
             <th class="text-center">Khách đến</th>
             <th class="text-center">Khách đến mua</th>
-            <th class="text-center">Khách đến k/mua</th>
+            <th class="text-center">Khách đến k.mua</th>
             <th class="text-center">Đơn chốt</th>
             <th class="text-center">Tỷ lệ<span class=""><br>chốt lịch</span></th>
             <th class="text-center">Tỷ lệ đến/SĐT</th>
             <th class="text-center">Tỷ lệ đến/lịch hẹn</th>
+            <th class="text-center">% Đến mua/lịch hẹn</th>
+            <th class="text-center">% Đến k.mua/lịch hẹn</th>
             <th class="text-center">Tỷ lệ<span class=""><br>chốt đơn</span></th>
             <th class="text-center">Tỷ lệ<span class=""><br>chốt đơn /SĐT</span></th>
             <th class="text-center">TB đơn</th>
@@ -28,9 +30,7 @@
             <th class="text-center">Doanh thu</th>
             <th class="text-center no-wrap">Thu nợ</th>
             <th class="text-center no-wrap">Thực thu</th>
-{{--            <th class="text-center no-wrap">Đơn chốt</th>--}}
-{{--            <th class="text-center">Doanh số<span class=""><br>sau CK</span></th>--}}
-{{--            <th class="text-center">Thực thu</th>--}}
+
         </tr>
         <tr class="number_index">
             <th class="text-center">(1)</th>
@@ -47,6 +47,8 @@
             <th class="text-center">(5)/(4)</th>
             <th class="text-center">(8)/(5)</th>
             <th class="text-center">(8)/(3)</th>
+            <th class="text-center">(8)/(3)1</th>
+            <th class="text-center">(8)/(3)2</th>
             <th class="text-center">(12)/(8)</th>
             <th class="text-center">(9)</th>
             <th class="text-center">(10)</th>
@@ -103,8 +105,10 @@
                     <td class="text-center pdr10">{{!empty($item->schedules_den) && !empty($item->customer_new) ? round($item->schedules_den/$item->customer_new*100,1):0}}%</td>
                     <td class="text-center pdr10">{{!empty($item->schedules_den) && !empty($item->schedules_new) ? round($item->schedules_den/$item->schedules_new*100,1):0}}%</td>
                     <td class="text-center pdr10">{{$item->order_new>0&&$item->schedules_den >0 ?round(($item->order_new/$item->schedules_den)*100,1):0}}%</td>
-                    <td class="text-center pdr10">{{$item->order_new>0&&$item->customer_new >0 ?round(($item->order_new/$item->customer_new)*100,1):0}}%</td>
-                    <td class="text-center pdr10">{{$item->order_new>0&&$item->detail_new >0 ?number_format($item->detail_new/$item->order_new):0}}</td>
+                    <td class="text-center pdr10">{{$item->schedules_new> 0 && $item->become_buy >0 ?round(($item->become_buy/$item->schedules_new)*100,1):0}}%</td>
+                    <td class="text-center pdr10">{{$item->schedules_new> 0 && $item->not_buy >0 ?round(($item->not_buy/$item->schedules_new)*100,1):0}}%</td>
+                    <td class="text-center pdr10">{{$item->order_new>0 && $item->customer_new >0 ?round(($item->order_new/$item->customer_new)*100,1):0}}%</td>
+                    <td class="text-center pdr10">{{$item->order_new>0 && $item->detail_new >0 ?number_format($item->detail_new/$item->order_new):0}}</td>
                     <td class="text-center pdr10">{{number_format($item->revenue_new)}}</td>
                     <td class="text-center pdr10">{{number_format($item->payment_new)}}</td>
                     <td class="text-center pdr10">{{number_format($item->detail_new - $item->payment_new)}}</td>
@@ -117,7 +121,6 @@
             <td class="text-center bold">Tổng cộng</td>
             <td class="text-center bold">{{@number_format($users->sum('call_center'))}}</td>
             <td class="text-center bold">{{@number_format($customer_new)}}</td>
-            {{--<td class="text-center bold">{{@number_format($comment_new)}}</td>--}}
             <td class="text-center bold">{{@number_format($schedules_new)}}</td>
             <td class="text-center bold">{{@number_format($all_schedules_den)}}</td>
             <td class="text-center bold">{{@number_format($users->sum('become_buy'))}}</td>
@@ -127,25 +130,14 @@
             <td class="text-center bold">{{!empty($all_schedules_den)&& !empty($customer_new)?round($all_schedules_den/$customer_new*100,1):0}}%</td>
             <td class="text-center bold">{{!empty($all_schedules_den)&& !empty($schedules_new)?round($all_schedules_den/$schedules_new*100,1):0}}%</td>
             <td class="text-center bold">{{!empty($all_schedules_den)&& !empty($order_new)?round($order_new/$all_schedules_den*100,1):0}}%</td>
+            <td class="text-center bold">{{!empty($schedules_new)&& !empty($users->sum('become_buy'))?round($users->sum('become_buy')/$all_schedules_den*100,1):0}}%</td>
+            <td class="text-center bold">{{!empty($schedules_new)&& !empty($users->sum('not_buy'))?round($users->sum('not_buy')/$all_schedules_den*100,1):0}}%</td>
             <td class="text-center bold">{{!empty($customer_new)&& !empty($order_new)?round($order_new/$customer_new*100,1):0}}%</td>
             <td class="text-center bold">{{!empty($all_detail_new)&& !empty($order_new)?number_format($all_detail_new/$order_new):0}}</td>
             <td class="text-center bold">{{@number_format($revenue_new)}}</td>
             <td class="text-center bold">{{@number_format($payment_new)}}</td>
             <td class="text-center bold">{{@number_format($all_detail_new - $payment_new)}}</td>
             <td class="text-center bold">{{@number_format($all_detail_new)}}</td>
-
-
-{{--            <td class="text-center bold">{{@number_format($order_old)}}</td>--}}
-{{--            <td class="text-center bold">{{@number_format($revenue_old)}}</td>--}}
-{{--            <td class="text-center bold">{{@number_format($detail_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($comment_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($all_payment - $all_detail_new - $payment_old)}}</td>--}}
-            {{--<td class="text-center bold">{{@number_format($all_payment - $all_detail_new)}}</td>--}}
-            {{--<td class="text-center bold">{{!empty($payment_old)&& !empty($order_old)?number_format($payment_old/$order_old):0}}</td>--}}
-            {{--<td class="bold">{{number_format($allTotal)}}</td>--}}
-            {{--<td class="bold">{{number_format($payment_new + $payment_old)}}</td>--}}
-            {{--<td class="bold">{{number_format(($all_payment - $payment_new - $payment_old)>0?$all_payment - $payment_new - $payment_old:0)}}</td>--}}
-            {{--<td class="bold">{{number_format($users->sum('all_payment'))}}</td>--}}
         </tr>
 
         </tbody>
