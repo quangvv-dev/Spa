05b96c91a72d95de6f0c457f6bc071162f11aa7f
quangphuong9685 <quangphuong9685@gmail.com>
2023-06-24 01:51:36 +0700

    cskh move

--- app/Http/Controllers/BE/DBController.php
+++ app/Http/Controllers/BE/DBController.php
@@ -19,51 +19,52 @@ class DBController extends Controller
 {
     public function index(Request $request)
     {
-        $param = $request->all();
-        $orders2 = PaymentHistory::where('price', '>', 0)
-            ->whereBetween('payment_date', ['2021-01-01 00:00:00', '2021-11-30 23:59:59'])->where('branch_id', $param['branch_id'])
-            ->with('order')
-            ->when(isset($param['type']), function ($q) use ($param) {
-                $q->onlyTrashed();
-            })->get();
 
-        Excel::create('Đơn hàng (' . date("d/m/Y") . ')', function ($excel) use ($orders2) {
-            $excel->sheet('Sheet 1', function ($sheet) use ($orders2) {
-                $sheet->cell('A1:P1', function ($row) {
-                    $row->setBackground('#008686');
-                    $row->setFontColor('#ffffff');
-                });
-                $sheet->freezeFirstRow();
-                $sheet->row(1, [
-                    'STT',
-                    'Ngày thanh toán',
-                    'Ngày tạo thanh toán',
-                    'Mã đơn',
-                    'Số tiền',
-                    'Tên KH',
-                    'SĐT',
-                    'Doanh số',
-                    'Doanh thu',
-                ]);
-                $i = 1;
-                if ($orders2) {
-                    foreach ($orders2 as $k => $ex) {
-                        $i++;
-                        $sheet->row($i, [
-                            @$k + 1,
-                            isset($ex->payment_date) ? date("d/m/Y", strtotime($ex->payment_date)) : '',
-                            isset($ex->created_at) ? date("d/m/Y", strtotime($ex->created_at)) : '',
-                            isset($ex->deleted_at) ? date("d/m/Y", strtotime($ex->deleted_at)) : '',
-                            @$ex->price,
-                            @$ex->order->customer->full_name,
-                            @$ex->order->customer->phone,
-                            @$ex->order->all_total,
-                            @$ex->order->gross_revenue,
-                        ]);
-                    }
-                }
-            });
-        })->export('xlsx');
+//        $param = $request->all();
+//        $orders2 = PaymentHistory::where('price', '>', 0)
+//            ->whereBetween('payment_date', ['2021-01-01 00:00:00', '2021-11-30 23:59:59'])->where('branch_id', $param['branch_id'])
+//            ->with('order')
+//            ->when(isset($param['type']), function ($q) use ($param) {
+//                $q->onlyTrashed();
+//            })->get();
+//
+//        Excel::create('Đơn hàng (' . date("d/m/Y") . ')', function ($excel) use ($orders2) {
+//            $excel->sheet('Sheet 1', function ($sheet) use ($orders2) {
+//                $sheet->cell('A1:P1', function ($row) {
+//                    $row->setBackground('#008686');
+//                    $row->setFontColor('#ffffff');
+//                });
+//                $sheet->freezeFirstRow();
+//                $sheet->row(1, [
+//                    'STT',
+//                    'Ngày thanh toán',
+//                    'Ngày tạo thanh toán',
+//                    'Mã đơn',
+//                    'Số tiền',
+//                    'Tên KH',
+//                    'SĐT',
+//                    'Doanh số',
+//                    'Doanh thu',
+//                ]);
+//                $i = 1;
+//                if ($orders2) {
+//                    foreach ($orders2 as $k => $ex) {
+//                        $i++;
+//                        $sheet->row($i, [
+//                            @$k + 1,
+//                            isset($ex->payment_date) ? date("d/m/Y", strtotime($ex->payment_date)) : '',
+//                            isset($ex->created_at) ? date("d/m/Y", strtotime($ex->created_at)) : '',
+//                            isset($ex->deleted_at) ? date("d/m/Y", strtotime($ex->deleted_at)) : '',
+//                            @$ex->price,
+//                            @$ex->order->customer->full_name,
+//                            @$ex->order->customer->phone,
+//                            @$ex->order->all_total,
+//                            @$ex->order->gross_revenue,
+//                        ]);
+//                    }
+//                }
+//            });
+//        })->export('xlsx');
 
     }
 }
