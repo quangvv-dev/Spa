906d714220223cfbd4b57080cfacbfe0e0436d0c
quangvv-dev <quangphuong9685@gmail.com>
2023-06-26 14:31:42 +0700

    fix qua han lic hen

--- app/Console/Commands/ScheduleQuaHan.php
+++ app/Console/Commands/ScheduleQuaHan.php
@@ -27,16 +27,6 @@ class ScheduleQuaHan extends Command
     protected $description = 'Command description';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
