280f023884a33acb026d23a2f63d43020cf671bd
quangphuong9685 <quangphuong9685@gmail.com>
2023-09-01 09:59:55 +0700

    check route mkt

--- app/Http/Resources/MarketingResource.php
+++ app/Http/Resources/MarketingResource.php
@@ -23,8 +23,8 @@ class MarketingResource extends JsonResource
             'orders'           => @(int)$this->orders,
             'schedules'        => @(int)$this->schedules,
             'percent_schedule' => !empty($this->contact) ? round(($this->schedules / $this->contact) *100 , 2) : 0,
-            'percent_order'    => !empty($this->contact) ? round($this->orders / $this->contact * 100, 2) : 0,
-            'avg'              => !empty($this->orders) ? round($this->gross_revenue / $this->orders, 2) : 0,
+            'percent_order'    => !empty($this->contact) ? round(($this->orders / $this->contact) *100 , 2) : 0,
+            'avg'              => !empty($this->orders) ? round(($this->gross_revenue / $this->orders) *100 , 2) : 0,
             'gross_revenue'    => @$this->gross_revenue,
         ];
         return $result;
