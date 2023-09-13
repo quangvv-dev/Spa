db07000c7e86a15666f76f0c08156814ff6271f4
quangvv-dev <quangphuong9685@gmail.com>
2023-09-13 15:20:35 +0700

    phan bo data

--- resources/views/customers/modal-update-account-manager.blade.php
+++ resources/views/customers/modal-update-account-manager.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="show-manager-account" tabindex="-1" role="dialog"  aria-hidden="true">
+<div class="modal fade" id="show-manager-account" role="dialog"  aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
