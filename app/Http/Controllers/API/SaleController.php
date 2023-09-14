891d7b79355af4f9d53deff35b386384d5cd1d27
quangvv-dev <quangphuong9685@gmail.com>
2023-09-14 11:01:43 +0700

    fix format dữ liệu

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -60,7 +60,7 @@ class SaleController extends BaseApiController
                 'totalNew'         => @$payments->firstWhere('id',$item->id)->totalNew??0,
                 'the_rest'         => @!empty($payments->firstWhere('id',$item->id)->the_rest)?(int)$payments->firstWhere('id',$item->id)->the_rest:0,
             ];
-            $result['percentOrder'] = !empty($result['phoneNew']) ? round($result['orderNew'] / $result['phoneNew'] * 100, 2) : 0;
+            $result['percentOrder'] = (int)$result['phoneNew']>0 ? round($result['orderNew'] / $result['phoneNew'] * 100, 2) : 0;
             $result['avg'] = (int)$result['phoneNew'] > 0  ? round((int)$result['totalNew'] / (int)$result['orderNew'] * 100, 2) : 0;
             return $result;
         });
