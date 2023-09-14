64bf0c75bb1b1d8044f2c27c10f66ee95eb04913
quangvv-dev <quangphuong9685@gmail.com>
2023-09-14 10:58:55 +0700

    fix format dữ liệu

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -61,7 +61,7 @@ class SaleController extends BaseApiController
                 'the_rest'         => @!empty($payments->firstWhere('id',$item->id)->the_rest)?(int)$payments->firstWhere('id',$item->id)->the_rest:0,
             ];
             $result['percentOrder'] = !empty($result['phoneNew']) ? round($result['orderNew'] / $result['phoneNew'] * 100, 2) : 0;
-            $result['avg'] = !empty($result['totalNew']) && !empty($result['phoneNew']) ? round($result['totalNew'] / $result['orderNew'] * 100, 2) : 0;
+            $result['avg'] = (int)$result['phoneNew'] > 0  ? round((int)$result['totalNew'] / (int)$result['orderNew'] * 100, 2) : 0;
             return $result;
         });
         return $this->responseApi(ResponseStatusCode::OK, 'SUCCESS', $users);
