058338b9389475fe85fb36836b6381d93498740b
quangvv-dev <quangphuong9685@gmail.com>
2023-09-15 10:43:58 +0700

    clear xh telesale cu

--- app/Services/SaleService.php
+++ app/Services/SaleService.php
@@ -135,7 +135,7 @@ class SaleService
                 'the_rest'         => @!empty($payments->firstWhere('id',$item->id)->the_rest)?(int)$payments->firstWhere('id',$item->id)->the_rest:0,
             ];
             $result['percentOrder'] = (int)$result['phoneNew']>0 ? round($result['orderNew'] / $result['phoneNew'] * 100, 2) : 0;
-            $result['avg'] = (int)$result['orderNew'] > 0  ? round((int)$result['totalNew'] / (int)$result['orderNew'] * 100, 2) : 0;
+            $result['avg'] = (int)$result['orderNew'] > 0  ? round((int)$result['totalNew'] / (int)$result['orderNew']) : 0;
             return $result;
         });
     }
