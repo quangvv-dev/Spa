b609dc33e8107bb38559f4d488200efe64d74165
quangphuong9685 <quangphuong9685@gmail.com>
2023-12-10 09:42:22 +0700

    status model

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -88,7 +88,23 @@ class Status extends Model
                 $query->where('c.telesales_id', $input['telesales']);
             })->when(isset($input['telesales']), function ($query) use ($input) {
                 $query->where('c.telesales_id', $input['telesales']);
-            })->orderBy('status.position', 'ASC')->groupBy('status.id')->get();
+            })->when(isset($input['data_time']) && $input['data_time'], function ($query) use ($input) {
+                $query->when($input['data_time'] == 'TODAY' ||
+                    $input['data_time'] == 'YESTERDAY', function ($q) use ($input) {
+                    $q->whereDate('c.created_at', getTime(($input['data_time'])));
+                })->when($input['data_time'] == 'THIS_WEEK' ||
+                        $input['data_time'] == 'LAST_WEEK' ||
+                        $input['data_time'] == 'THIS_MONTH' ||
+                        $input['data_time'] == 'LAST_MONTH', function ($q) use ($input) {
+                        $q->whereBetween('c.created_at', getTime(($input['data_time'])));
+                    });
+            })->when(isset($input['start_date']) && isset($input['end_date']), function ($q) use ($input) {
+                $q->whereBetween('c.created_at', [
+                    Functions::yearMonthDay($input['start_date']) . " 00:00:00",
+                    Functions::yearMonthDay($input['end_date']) . " 23:59:59",
+                ]);
+            })
+            ->orderBy('status.position', 'ASC')->groupBy('status.id')->get();
 
         return $data;
     }
