83c2e2e3fcce4010f48ab0e8aaf125c46d401494
quangvv-dev <quangphuong9685@gmail.com>
2024-02-27 10:11:20 +0700

    chanpoint

--- app/Http/Controllers/BE/OrderController.php
+++ app/Http/Controllers/BE/OrderController.php
@@ -573,7 +573,7 @@ class OrderController extends Controller
             $paymentHistory = PaymentHistoryService::create($input, $id);
             unset($input['is_debt']);
             if ($paymentHistory->payment_type != 3) {
-                $point = $paymentHistory->price / StatusCode::EXCHANGE_POINT * StatusCode::EXCHANGE_MONEY;
+                $point = $customer->wallet;
             } else {
                 $point = ($customer->wallet - $paymentHistory->price) > 0 ? $customer->wallet - $paymentHistory->price : 0;
             }
