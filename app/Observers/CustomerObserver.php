b6ba71149f80a3343f6729a3c3a67eab89349944
quangvv-dev <quangphuong9685@gmail.com>
2024-04-01 10:44:10 +0700

    log chinh sua

--- app/Observers/CustomerObserver.php
+++ app/Observers/CustomerObserver.php
@@ -26,19 +26,18 @@ class CustomerObserver
 
     public function updated(Customer $customer)
     {
-        $changedAttributes = $customer->getDirty();
-        // Loại bỏ trường 'updated_at' khỏi mảng sự thay đổi
-        unset($changedAttributes['updated_at'], $changedAttributes['is_gioithieu']);
-
-        // Kiểm tra sự thay đổi của các trường khác
-        if (count($changedAttributes)) {
-            $customer->groupComments()->create([
-                'customer_id' => $customer->id,
-                'branch_id' => $customer->branch_id,
-                'status_id' => $customer->status_id,
-                'user_id' => 0,
-                'messages' => "<span class='bold text-danger'>CHỈNH SỬA KH: </span> " . Auth::user()->full_name . " thao tác lúc " . date('H:i d-m-Y'),
-            ]);
-        }
+//        $changedAttributes = $customer->getDirty();
+//        // Loại bỏ trường 'updated_at' khỏi mảng sự thay đổi
+//        unset($changedAttributes['updated_at'], $changedAttributes['is_gioithieu']);
+//        // Kiểm tra sự thay đổi của các trường khác
+//        if (count($changedAttributes)) {
+//            $customer->groupComments()->create([
+//                'customer_id' => $customer->id,
+//                'branch_id' => $customer->branch_id,
+//                'status_id' => $customer->status_id,
+//                'user_id' => 0,
+//                'messages' => "<span class='bold text-danger'>CHỈNH SỬA KH: </span> " . Auth::user()->full_name . " thao tác lúc " . date('H:i d-m-Y'),
+//            ]);
+//        }
     }
 }
