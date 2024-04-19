0c37c149198fe8a51d51a75f84a4bd8915b3293a
quangvv-dev <quangphuong9685@gmail.com>
2024-04-19 18:03:50 +0700

    tab moi

--- resources/views/users/ajax.blade.php
+++ resources/views/users/ajax.blade.php
@@ -46,7 +46,7 @@
                             <input data-id="{{$user->id}}" name="checkbox" class="check" type="checkbox" {{$user->active==\App\Constants\StatusCode::ON?'checked':''}}>
                             <span class="slider round"></span>
                         </label>
-                        <a title="sửa tài khoản" class="btn" href="{{ route('users.edit', $user->id) }}"><i class="fas fa-edit"></i></a>
+                        <a target="_blank" title="sửa tài khoản" class="btn" href="{{ route('users.edit', $user->id) }}"><i class="fas fa-edit"></i></a>
                         @if (Auth::user()->department_id == \App\Constants\DepartmentConstant::ADMIN)
                             <a title="Xóa tài khoản" class="btn delete" href="javascript:void(0)"
                                data-url="{{ route('users.destroy', $user->id) }}"><i class="fas fa-trash-alt"></i></a>
