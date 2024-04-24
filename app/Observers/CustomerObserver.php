33e6e5f3bc2e13c41d94df956b31906157e366e5
quangvv-dev <quangphuong9685@gmail.com>
2024-04-24 14:34:36 +0700

    observer updted customer

--- app/Observers/CustomerObserver.php
+++ app/Observers/CustomerObserver.php
@@ -57,14 +57,14 @@ class CustomerObserver
         // Kiểm tra sự thay đổi của các trường khác
         if (count($changedAttributes)) {
             $text = '';
-            if (!empty($changedAttributes['mkt_id'])) {
-                $text = $text . ' <span class="text-purple">MKT: ' . User::find($oldData['mkt_id'])->full_name . ' --> ' . User::find($changedAttributes['mkt_id'])->full_name . '</span>';
+            if (!empty(@$changedAttributes['mkt_id'])) {
+                $text = $text . ' <span class="text-purple">MKT: ' . @User::find($oldData['mkt_id'])->full_name . ' --> ' . @User::find($changedAttributes['mkt_id'])->full_name . '</span>';
             }
-            if (!empty($changedAttributes['telesales_id'])) {
-                $text = $text . ' <span class="text-info">| Sale: ' . User::find($oldData['telesales_id'])->full_name . ' --> ' . User::find($changedAttributes['telesales_id'])->full_name . '</span>';
+            if (!empty(@$changedAttributes['telesales_id'])) {
+                $text = $text . ' <span class="text-info">| Sale: ' . @User::find($oldData['telesales_id'])->full_name . ' --> ' . @User::find($changedAttributes['telesales_id'])->full_name . '</span>';
             }
-            if (!empty($changedAttributes['status_id'])) {
-                $text = $text . ' <span class="text-green">| Trạng thái: ' . Status::find($oldData['status_id'])->name . ' --> ' . Status::find($changedAttributes['status_id'])->name . '</span>';
+            if (!empty(@$changedAttributes['status_id'])) {
+                $text = $text . ' <span class="text-green">| Trạng thái: ' . @Status::find($oldData['status_id'])->name . ' --> ' . @Status::find($changedAttributes['status_id'])->name . '</span>';
             }
             if (!empty($text)) {
                 $customer->groupComments()->create([
