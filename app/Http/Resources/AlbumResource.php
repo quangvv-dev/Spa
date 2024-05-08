c33d205dbd08c717625a1151f03c8c8d2b4358c9
quangvv-dev <quangphuong9685@gmail.com>
2024-05-08 15:05:00 +0700

    an sdt hs khac hang

--- app/Http/Resources/AlbumResource.php
+++ app/Http/Resources/AlbumResource.php
@@ -15,15 +15,16 @@ class AlbumResource extends JsonResource
     public function toArray($request)
     {
 
+        $phone = $request->jwtUser->permission('phone.open') ? @$this->customer->phone : str_limit($this->customer->phone, 7, 'xxx');
         return [
 //            'id'               => @$this->id,
             'customer_id'      => @$this->customer->id,
             'customer_name'    => @$this->customer->full_name,
-            'phone'            => @$this->customer->phone,
+            'phone'            => $phone,
             'account_code'     => @$this->customer->account_code,
             'title'            => @$this->title,
             'branch_id'        => @$this->branch_id,
-            'service_text'           => @$this->service_text,
+            'service_text'     => @$this->service_text,
             'branch'           => @$this->branch->name,
             'images'           => @json_decode($this->images),
         ];
