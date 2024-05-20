9d288a4459ffd8ddf561f572f53f22dedfefcfd7
quangvv-dev <quangphuong9685@gmail.com>
2024-05-20 14:05:48 +0700

    fix lĩch hen

--- resources/views/schedules/home2.blade.php
+++ resources/views/schedules/home2.blade.php
@@ -134,9 +134,10 @@
                             // code block
                         }
                         let typeText = value.type == 1?'Lịch mới':(value.type == 2?'Tái khám':'Liệu trình');
+                        let categoryText = value.category.name??'';
                         b.push({
                             id: value.id,
-                            title: typeText+': '+value.category.name+', ' + value.customer.full_name + ', SĐT: ' + (hidden_phone == true ? value.customer.phone : value.customer.phone.slice(0, 7) + 'xxx') + ' Lưu ý: ' + value.note,
+                            title: typeText+': '+categoryText+', ' + value.customer.full_name + ', SĐT: ' + (hidden_phone == true ? value.customer.phone : value.customer.phone.slice(0, 7) + 'xxx') + ' Lưu ý: ' + value.note,
                             start: value.date + 'T' + value.time_from + ':00',
                             end: value.date + 'T' + value.time_to + ':00',
                             color: col,
@@ -320,9 +321,10 @@
                     var b = [];
                     var col = chooseColor(data.status);
                     let typeText = data.type == 1?'Lịch mới':(data.type == 2?'Tái khám':'Liệu trình');
+                    let categoryText = data.category.name??'';
                     b.push({
                         id: data.id,
-                        title: typeText+': '+data.category.name+', ' + data.customer.full_name + ', SĐT: ' + (hidden_phone == true ? data.customer.phone : data.customer.phone.slice(0, 7) + 'xxx') + ' Lưu ý: ' + data.note,
+                        title: typeText+': '+categoryText+', ' + data.customer.full_name + ', SĐT: ' + (hidden_phone == true ? data.customer.phone : data.customer.phone.slice(0, 7) + 'xxx') + ' Lưu ý: ' + data.note,
                         description: data.note,
                         start: data.date + 'T' + data.time_from + ':00',
                         end: data.date + 'T' + data.time_to + ':00',
