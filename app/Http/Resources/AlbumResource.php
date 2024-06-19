dcd32c48953a18d77e5b96aae967096de98c2018
quangphuong9685 <quangphuong9685@gmail.com>
2024-06-19 23:42:21 +0700

    album phan quỳn

--- app/Http/Resources/AlbumResource.php
+++ app/Http/Resources/AlbumResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Constants\DepartmentConstant;
+use App\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AlbumResource extends JsonResource
@@ -9,22 +11,23 @@ class AlbumResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function toArray($request)
     {
+        $user = User::find($request->jwtUser->id);
 
         return [
-//            'id'               => @$this->id,
-            'customer_id'      => @$this->customer->id,
-            'customer_name'    => @$this->customer->full_name,
-            'phone'            => @$this->customer->phone,
-            'title'            => @$this->title,
-            'branch_id'        => @$this->branch_id,
-            'service_text'           => @$this->service_text,
-            'branch'           => @$this->branch->name,
-            'images'           => @json_decode($this->images),
+            'customer_id'   => @$this->customer->id,
+            'customer_name' => @$this->customer->full_name,
+            'phone'         => @$this->customer->phone,
+            'title'         => @$this->title,
+            'branch_id'     => @$this->branch_id,
+            'service_text'  => @$this->service_text,
+            'branch'        => @$this->branch->name,
+            'images'        => in_array($user->department_id, [DepartmentConstant::ADMIN, DepartmentConstant::KE_TOAN]) ? @json_decode($this->images) : [],
         ];
     }
 }
