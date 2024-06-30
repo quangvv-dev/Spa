11fd8559d5834c0120f579ddf8cbc63ff5888a82
quangphuong9685 <quangphuong9685@gmail.com>
2024-06-30 12:29:01 +0700

    command deleted task <> customer_status

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,14 +30,14 @@ class Kernel extends ConsoleKernel
         $schedule->command('update:customer_status')->daily();
         $schedule->command('notification:schedules_quahan')->daily();
         $schedule->command('delete:task')->dailyAt('07:00');
-        $schedule->command('sms:revenue')->dailyAt('07:00');
+//        $schedule->command('sms:revenue')->dailyAt('07:00');
         $schedule->command('job:action')->dailyAt('08:00');
-        $schedule->command('otp:refresh')->dailyAt('01:00');
+//        $schedule->command('otp:refresh')->dailyAt('01:00');
         $schedule->command('deleted:Call')->dailyAt('01:00');
+        $schedule->command('tasks:delete-mismatched')->dailyAt('00:30');
 
         $schedule->command('expired:search')->everyMinute();
         $schedule->command('move_customer:search')->everyMinute();
-//        $schedule->command('notification:action')->everyMinute();
     }
 
     /**
