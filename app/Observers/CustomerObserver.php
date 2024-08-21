7dc0203dd696e1789b8e5ded123caf22d5c87b53
quangvv-dev <quangphuong9685@gmail.com>
2024-08-21 17:48:35 +0700

    fix tao moi kH v4

--- app/Observers/CustomerObserver.php
+++ app/Observers/CustomerObserver.php
@@ -34,7 +34,7 @@ class CustomerObserver
         if (!empty(Auth::user())){
             dd(1);
         }else{
-            dd(2);
+            dd(@User::find($customer->carepage_id)->full_name);
 
         }
         if (!empty($customer->facebook)) {
@@ -50,7 +50,7 @@ class CustomerObserver
             'branch_id'   => $customer->branch_id,
             'status_id'   => $customer->status_id,
             'user_id'     => Auth::user()->id ?? $customer->carepage_id,
-            'messages'    => "<span class='bold text-azure'>Tạo mới KH: </span> " . Auth::user()->full_name??User::find($customer->carepage_id)->full_name . " thao tác lúc " . date('H:i d-m-Y'),
+            'messages'    => "<span class='bold text-azure'>Tạo mới KH: </span> " . Auth::user()->full_name??@User::find($customer->carepage_id)->full_name . " thao tác lúc " . date('H:i d-m-Y'),
         ]);
     }
 
