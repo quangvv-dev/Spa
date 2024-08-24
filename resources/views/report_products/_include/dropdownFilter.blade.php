d4fdf720284f1719a2f1871a8c5b2a24376100b6
quangphuong9685 <quangphuong9685@gmail.com>
2024-08-24 21:29:17 +0700

    filter souce & tp

--- resources/views/report_products/_include/dropdownFilter.blade.php
+++ resources/views/report_products/_include/dropdownFilter.blade.php
@@ -1,6 +1,6 @@
 <div class="row isShowView" style="display:none; padding-top: 0.5rem">
     <div class="col-md-2 col-xs-12">
-        <select name="source" class="form-control select2">
+        <select name="source_id" class="form-control select2">
             <option value="">Nguồn khách hàng</option>
             @foreach($source as $k=> $item)
                 <option value="{{$k}}">{{ $item}}</option>
