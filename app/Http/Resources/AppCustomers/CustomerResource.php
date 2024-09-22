b92ebed5a40d48df1e2cd1322f48487ecd2b92ac
quangphuong9685 <quangphuong9685@gmail.com>
2024-09-22 22:22:33 +0700

    fix api call

--- app/Http/Resources/AppCustomers/CustomerResource.php
+++ app/Http/Resources/AppCustomers/CustomerResource.php
@@ -21,23 +21,24 @@ class CustomerResource extends JsonResource
 //            $user = User::find($request->jwtUser->id);
 //            $phone = @$user->permission('phone.open') ? $phone : str_limit($phone, 7, 'xxx');
 //        }
-        if ($request->type =='full_data'){
+        if ($request->type == 'full_data') {
             return [
-                'id'           => @$this->id,
-                'full_name'    => @$this->full_name,
-                'phone'        => $phone,
-                'telesales_id' => @$this->telesales_id,
-                'mkt_id'       => @$this->mkt_id,
-                'gender'       => @$this->gender,
-                'status_id'    => @$this->status_id,
-                'source_id'    => @$this->source_id,
-                'description'  => @$this->description,
-                'branch_id'    => @$this->branch_id,
-                'branch_name'  => @$this->branch->name,
-                'status_name'  => @$this->status->name,
-                'group_array'  => @$this->group_array,
+                'id'               => @$this->id,
+                'full_name'        => @$this->full_name,
+                'phone'            => $phone,
+                'phone_click2call' => $phone,
+                'telesales_id'     => @$this->telesales_id,
+                'mkt_id'           => @$this->mkt_id,
+                'gender'           => @$this->gender,
+                'status_id'        => @$this->status_id,
+                'source_id'        => @$this->source_id,
+                'description'      => @$this->description,
+                'branch_id'        => @$this->branch_id,
+                'branch_name'      => @$this->branch->name,
+                'status_name'      => @$this->status->name,
+                'group_array'      => @$this->group_array,
             ];
-        }else{
+        } else {
             return [
                 'id'           => @$this->id,
                 'full_name'    => @$this->full_name,
