02a06356abced4312648a3d7ae1b05d2b880cd58
quangvv-dev <quangphuong9685@gmail.com>
2024-09-26 09:06:10 +0700

    change click2call async

--- app/Http/Controllers/API/CallController.php
+++ app/Http/Controllers/API/CallController.php
@@ -207,7 +207,7 @@ class CallController extends BaseApiController
         }
         $accessToken = '88d4b615ef0c4afdb3485fce4d90a300-ZDZmZTRkMmUtM2UxOS00ZjMzLWIzOTUtOGEzNmRkMWQ2Mzc5';
         $header = ['Authorization' => 'Bearer ' . $accessToken];
-        $url = 'https://api.mobilesip.vn/v1/click2call?ext=' . $user->caller_number . '&phone=' . $this->encryptedPhoneNumber($request->phone) . '&is_encode=true&encrypt_method=aes';
+        $url = 'https://api.mobilesip.vn/v1/click2call/async?ext=' . $user->caller_number . '&phone=' . $this->encryptedPhoneNumber($request->phone) . '&is_encode=true&encrypt_method=aes';
         try {
             $response = GuzzleHttpCall($url, 'GET', $header);
             return $this->responseApi(ResponseStatusCode::OK, 'Kết nối cuộc gọi thành công!', $response);
