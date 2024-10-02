ae8f36101e784904f4a8974717c9225e72fef286
quangvv-dev <quangphuong9685@gmail.com>
2024-10-02 14:53:14 +0700

    cai dat thoi gian

--- resources/views/customers/modal_view.blade.php
+++ resources/views/customers/modal_view.blade.php
@@ -1,11 +1,12 @@
 <div class="modal fade modal-custom" id="view_chat" role="dialog">
+{{--    data-toggle="modal" data-backdrop="static" data-keyboard="false"--}}
     <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content" >
             <div class="modal-header" style="padding-bottom: 0">
                 <h4 class="modal-title-custom ni linear-text fs-24" style="font-weight: 900">Trao đổi nhanh</h4>
                 <i style="color: red"> </i>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close" data-dismiss="modal" style="color: #fff">&times;</button>
             </div>
             <div class="modal-body customer-chat">
 
