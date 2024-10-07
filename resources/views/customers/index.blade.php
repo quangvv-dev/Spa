fcabbd663703c174a573ba36bec1a46c202cda9a
quangphuong9685 <quangphuong9685@gmail.com>
2024-10-08 00:15:43 +0700

    enum errorSip

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -69,7 +69,7 @@
                     console.log('Microphone đã được kết nối');
                 })
                 .catch(function (err) {
-                    alertify.warning('Microphone không tìm thấy');
+                    alertify.warning('Chưa kết nối Microphone !');
                 });
         }
 
