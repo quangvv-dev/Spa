34a0bee49a02104bae68f6dd0198a771e03984f8
quangvv-dev <quangphuong9685@gmail.com>
2024-10-07 11:06:37 +0700

    phone format

--- resources/views/layout/_script.blade.php
+++ resources/views/layout/_script.blade.php
@@ -32,6 +32,7 @@
             $(document).on('click','#callButton',function () {
                 let phone = $(this).data('phone');
                 phone = phone.split(' ').join('');
+                console.log('phoneFormat',phone);
                 pitelSDK.call(phone, {
                     extraHeaders: ['x-PROCESS-ID: 123']
                 });
