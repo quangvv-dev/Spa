9ec03e01ffb7ab6f48bdca5ee1abf23869aa64c1
quangphuong9685 <quangphuong9685@gmail.com>
2024-10-29 23:22:08 +0700

    loading thanh toan

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -308,7 +308,7 @@
                             customer_id: customer_id,
                         }
                     }).done(function () {
-                        // window.location.reload();
+                        window.location.reload();
                     });
                 });
 
